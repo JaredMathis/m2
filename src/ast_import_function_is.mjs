7a5421e4c6b57da2c99f9d9e08325c34bbac2fad
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:32:42 -0500

    Sun Jan 29 2023 18:32:42 GMT-0500 (Eastern Standard Time)

--- src/ast_import_function_is.mjs
+++ src/ast_import_function_is.mjs
@@ -1,10 +1,12 @@
 export function ast_import_function_is(b) {
     let result = false;
+    let name;
     if (b.type === 'ImportDeclaration' && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
         let first = b.specifiers[0];
-        if (first.imported.name === first.local.name) {
+        name = first.imported.name ;
+        if (name === first.local.name) {
             result = true;
         }
     }
-    return result;
+    return {result, name};
 }
