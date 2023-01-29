613974137f3562f467ecfdde66b9713b2f42b4b9
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:16:50 -0500

    Sun Jan 29 2023 17:16:50 GMT-0500 (Eastern Standard Time)

---
+++ src/command.mjs
@@ -0,0 +1,6 @@
+
+
+let function_name = process.argv[2];
+let function_arguments = process.argv.slice(3);
+
+console.log({function_name, function_arguments})
--- src/function_new.mjs
+++ src/function_new.mjs
@@ -1,6 +1,6 @@
 import { file_write } from "./file_write.mjs";
 
-export function function_new(name) {
-    file_write(`${name}.mjs`, `export function ${name}() {
+export async function function_new(name) {
+    await file_write(`${name}.mjs`, `export function ${name}() {
 }`);
 }
