5068267833bf88845e7f8894820ef7ea075647d3
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:31:27 -0500

    Sun Jan 29 2023 18:31:27 GMT-0500 (Eastern Standard Time)

--- src/ast_imports.mjs
+++ src/ast_imports.mjs
@@ -4,13 +4,18 @@ import { for_each } from "./for_each.mjs";
 export function ast_imports(ast) {
     let {body} = ast;
     for_each(body, b => {
-        let result = false;
-        if (b.type === 'ImportDeclaration' && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
-            let first = b.specifiers[0];
-            if (first.imported.name === first.local.name) {
-                result = true;
-            }
-        }
+        let result = ast_import_function_is(b);
         log(result);
     })
+}
+
+function ast_import_function_is(b) {
+    let result = false;
+    if (b.type === 'ImportDeclaration' && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
+        let first = b.specifiers[0];
+        if (first.imported.name === first.local.name) {
+            result = true;
+        }
+    }
+    return result;
 }
