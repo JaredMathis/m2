7b6255ee047a13fe0359f97e873b002d574aadc6
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:46:42 -0500

    noop,refactor

--- src/ast_identifiers.mjs
+++ src/ast_identifiers.mjs
@@ -1,9 +1,7 @@
 import { ast_visit } from './ast_visit.mjs';
 export function ast_identifiers(ast) {
-    const each =
-        node => {
-        };
-    ast_visit(ast, each);
+    ast_visit(ast, node => {
+    });
 }
 
 
