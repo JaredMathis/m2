7570d7a4bd48e8cdb174eb9edfb3bc2e1c9b8b19
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:54:31 -0500

    noop,progress

--- src/ast_identifiers.mjs
+++ src/ast_identifiers.mjs
@@ -2,15 +2,17 @@ import { ast_identifier_is } from './ast_identifier_is.mjs';
 import { ast_visit } from './ast_visit.mjs';
 export function ast_identifiers(ast) {
     let filter = ast_identifier_is;
+    return ast_filter(ast);
+}
+
+function ast_filter(ast) {
     let result = [];
     ast_visit(ast, v => {
-        let {node} = v;
+        let { node } = v;
         if (result(node)) {
             result.push(node);
         }
     });
     return result;
 }
-
-
 
