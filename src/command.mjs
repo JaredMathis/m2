0673ebf42c010108aa82f4a0bfb937e4c914788f
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:17:27 -0500

    Sun Jan 29 2023 17:17:27 GMT-0500 (Eastern Standard Time)

--- src/command.mjs
+++ src/command.mjs
@@ -1,3 +1,4 @@
 let function_name = process.argv[2];
 let function_arguments = process.argv.slice(3);
+let _function = await import(function_name + '.mjs');
 console.log({function_name, function_arguments})
