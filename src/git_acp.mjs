3eb5c52624d7606d2fa9daf427ccd8ba4aed64b8
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:02:14 -0500

    Sun Jan 29 2023 18:02:14 GMT-0500 (Eastern Standard Time)

--- src/git_acp.mjs
+++ src/git_acp.mjs
@@ -1,9 +1,10 @@
-import {run} from './run.mjs'
+import {run} from './run.mjs';
+import {log} from './log.mjs';
 
 await run('git add *');
 try {
     await run('git commit -m "' + new Date() + "\"");
     await run('git push');
 } catch (e) {
-    console.log('Git commit/push errored. Maybe there was nothing to commit');
+    log('Git commit/push errored. Maybe there was nothing to commit');
 }
