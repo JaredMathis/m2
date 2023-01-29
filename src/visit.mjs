5b8b16017ad05562bc9a6052acd0f40960b04f39
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:48:20 -0500

    noop,progress

--- src/visit.mjs
+++ src/visit.mjs
@@ -1,3 +1,11 @@
+import { for_each } from "./for_each.mjs";
+
 export function visit(item, valid_child, for_each_child) {
-    
+    for_each(item, child => {
+        if (!valid_child(child)) {
+            return;
+        }
+        for_each_child(child);
+        visit(child, valid_child, for_each_child);
+    })
 }
