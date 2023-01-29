a37d27fb1fe7b9ff94c8da90fe6925a4e580aae4
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:23:43 -0500

    Sun Jan 29 2023 18:23:43 GMT-0500 (Eastern Standard Time)

--- src/js_parse.mjs
+++ src/js_parse.mjs
@@ -1,4 +1,4 @@
 import * as acorn from "acorn";
 export function js_parse(text) {
-    return acorn.parse(text, {sourceType: "module"});
+    return acorn.parse(text, {sourceType: "module", ecmaVersion: '2020'});
 }
