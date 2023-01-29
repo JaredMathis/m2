eddc06d4d89f92622a8f53493fa540feeb4087d0
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:16:07 -0500

    Sun Jan 29 2023 18:16:07 GMT-0500 (Eastern Standard Time)

--- src/file_read.mjs
+++ src/file_read.mjs
@@ -1,3 +1,6 @@
-export function file_read() {
-    
+import fs from 'fs';
+
+export async function file_read(file_path) {
+    let result = await fs.promises.readFile(file_path, 'utf8');
+    return result;
 }
