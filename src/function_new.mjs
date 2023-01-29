0f10368ae2f9e79f4cf6c2cb7a790c9d7f1b736d
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:35:41 -0500

    Sun Jan 29 2023 17:35:41 GMT-0500 (Eastern Standard Time)

--- src/function_new.mjs
+++ src/function_new.mjs
@@ -1,7 +1,7 @@
 import { file_write } from "./file_write.mjs";
 
 export async function function_new(name) {
-    const file_path = `${name}.mjs`;
+    const file_path = path_join([`src`, `${name}.mjs`]);
     await file_write(file_path, `export function ${name}() {
 }`);
 }
---
+++ src/path_join.mjs
@@ -0,0 +1,5 @@
+import path from "path";
+
+export function path_join(paths) {
+    return path.join(paths)
+}
