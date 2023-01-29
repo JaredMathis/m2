eecc24c05b7361c422d4c00cf5fd54495ca6c745
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:51:22 -0500

    Sun Jan 29 2023 17:51:22 GMT-0500 (Eastern Standard Time)

--- src/file_exists.mjs
+++ src/file_exists.mjs
@@ -1,2 +1,6 @@
-export function file_exists() {
+import fs from 'fs';
+export function file_exists(file_path) {
+    return fs.promises.access(file_path, fs.constants.F_OK)
+           .then(() => true)
+           .catch(() => false)
 }
