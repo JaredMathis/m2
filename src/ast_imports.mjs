e84e7850e4f23b96c29fa82ba38c6090fe73c538
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:31:09 -0500

    Sun Jan 29 2023 18:31:08 GMT-0500 (Eastern Standard Time)

--- src/ast_imports.mjs
+++ src/ast_imports.mjs
@@ -4,8 +4,13 @@ import { for_each } from "./for_each.mjs";
 export function ast_imports(ast) {
     let {body} = ast;
     for_each(body, b => {
+        let result = false;
         if (b.type === 'ImportDeclaration' && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
-            log(b.specifiers[0]);
+            let first = b.specifiers[0];
+            if (first.imported.name === first.local.name) {
+                result = true;
+            }
         }
+        log(result);
     })
 }
