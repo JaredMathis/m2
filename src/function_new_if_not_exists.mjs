80e5ecb18b6418f240dd4c5ffb159a6292b0aa02
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:55:20 -0500

    Sun Jan 29 2023 17:55:20 GMT-0500 (Eastern Standard Time)

---
+++ src/function_new_if_not_exists.mjs
@@ -0,0 +1,8 @@
+import { function_new } from "./function_new.mjs";
+import { function_exists } from "./function_exists.mjs";
+
+export async function function_new_if_not_exists(function_name) {
+    if (!await function_exists(function_name)) {
+        await function_new(function_name);
+    }
+}
--- src/function_open.mjs
+++ src/function_open.mjs
@@ -1,6 +1,5 @@
-import { function_new } from "./function_new.mjs";
 import { vs_code_function_open } from "./vs_code_function_open.mjs";
-import { function_exists } from "./function_exists.mjs";
+import { function_new_if_not_exists } from "./function_new_if_not_exists.mjs";
 
 export async function function_open(function_name) {
     await function_new_if_not_exists(function_name);
@@ -8,9 +7,4 @@ export async function function_open(function_name) {
 }
 
 
-async function function_new_if_not_exists(function_name) {
-    if (!await function_exists(function_name)) {
-        await function_new(function_name);
-    }
-}
 
