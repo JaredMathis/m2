2f497040376fddb70e96e3359ed67a5d0c81c2c4
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:59:03 -0500

    function_imports,function_imports

--- src/ast_identifiers.mjs
+++ src/ast_identifiers.mjs
@@ -1,7 +1,9 @@
 import { ast_filter } from './ast_filter.mjs';
 import { ast_identifier_is } from './ast_identifier_is.mjs';
 export function ast_identifiers(ast) {
-    return ast_filter(ast, ast_identifier_is);
+    let filtered = ast_filter(ast, ast_identifier_is);
+    let mapped = filtered.map(i => i.name);
+    return mapped;
 }
 
 
