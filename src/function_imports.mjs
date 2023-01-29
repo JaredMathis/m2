a5ac8e526ca70da76d6ded9018b4bcfddfc350da
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:24:20 -0500

    function_imports,function_imports

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -1,6 +1,8 @@
 import { ast_imports } from './ast_imports.mjs';
 import {function_ast_get} from './function_ast_get.mjs';
+import {log} from './log.mjs';
 export async function function_imports(function_name) {
     let ast = await function_ast_get(function_name);
+    log(ast);
     let imports_existing = ast_imports(ast);
 }
