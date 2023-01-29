1ad4652d2c7a7d68d08ec76b373a00befd4a2523
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:55:53 -0500

    noop,progress

--- src/ast_import_function_is.mjs
+++ src/ast_import_function_is.mjs
@@ -1,7 +1,9 @@
+import { ast_node_type_is } from "./ast_node_type_is.mjs";
+
 export function ast_import_function_is(b) {
     let success = false;
     let name;
-    if (b.type === 'ImportDeclaration' && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
+    if (ast_node_type_is('ImportDeclaration') && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
         let first = b.specifiers[0];
         name = first.imported.name ;
         if (name === first.local.name) {
