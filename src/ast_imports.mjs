55db31958e87ee49af54fd8d3625ac30164f7b72
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:29:33 -0500

    function_imports,function_imports

--- src/ast_imports.mjs
+++ src/ast_imports.mjs
@@ -4,8 +4,8 @@ import { for_each } from "./for_each.mjs";
 export function ast_imports(ast) {
     let {body} = ast;
     for_each(body, b => {
-        if (b.type === 'ImportDeclaration') {
-            log(b);
+        if (b.type === 'ImportDeclaration' && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
+            log(b.specifiers[0]);
         }
     })
 }
--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -3,6 +3,5 @@ import {function_ast_get} from './function_ast_get.mjs';
 import {log} from './log.mjs';
 export async function function_imports(function_name) {
     let ast = await function_ast_get(function_name);
-    log(ast);
     let imports_existing = ast_imports(ast);
 }
