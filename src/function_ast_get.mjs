0425a67502226757ed28b850ea52096a90d49b17
JaredMathis <jared.mathis@native.consulting>
2023-01-29 18:10:22 -0500

    Sun Jan 29 2023 18:10:22 GMT-0500 (Eastern Standard Time)

--- src/function_ast_get.mjs
+++ src/function_ast_get.mjs
@@ -3,5 +3,5 @@ import {js_parse} from './js_parse.mjs';
 export async function function_ast_get(function_name) {
     let text = await file_read(function_name);
     let ast = js_parse(text);
-    return text;
+    return ast;
 }
