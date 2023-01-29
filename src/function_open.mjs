2c4a35fedb420d329d7e2f6d34402485dfa652b3
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:54:04 -0500

    Sun Jan 29 2023 17:54:04 GMT-0500 (Eastern Standard Time)

--- src/function_open.mjs
+++ src/function_open.mjs
@@ -4,8 +4,7 @@ import { function_new } from "./function_new.mjs";
 import { vs_code_function_open } from "./vs_code_function_open.mjs";
 
 export async function function_open(function_name) {
-    const file_path = function_path_get(function_name);
-    if (!await file_exists(file_path)) {
+    if (!await file_exists(function_path_get(function_name))) {
         await function_new(function_name);
     }
     await vs_code_function_open(function_name);
