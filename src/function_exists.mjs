1dc084a0e3c7f95b6bececd0ed44a5bf0a93f4f4
JaredMathis <jared.mathis@native.consulting>
2023-01-29 17:54:43 -0500

    Sun Jan 29 2023 17:54:43 GMT-0500 (Eastern Standard Time)

---
+++ src/function_exists.mjs
@@ -0,0 +1,6 @@
+import { function_path_get } from "./function_path_get.mjs";
+import { file_exists } from "./file_exists.mjs";
+
+export async function function_exists(function_name) {
+    return await file_exists(function_path_get(function_name));
+}
--- src/function_open.mjs
+++ src/function_open.mjs
@@ -1,11 +1,12 @@
-import { function_path_get } from "./function_path_get.mjs";
-import { file_exists } from "./file_exists.mjs";
 import { function_new } from "./function_new.mjs";
 import { vs_code_function_open } from "./vs_code_function_open.mjs";
+import { function_exists } from "./function_exists.mjs";
 
 export async function function_open(function_name) {
-    if (!await file_exists(function_path_get(function_name))) {
+    if (!await function_exists(function_name)) {
         await function_new(function_name);
     }
     await vs_code_function_open(function_name);
 }
+
+
