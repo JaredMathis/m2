0de672f2925f2d73b067a8339972959ecd0a2de2
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:52:42 -0500

    progress

--- src/command.mjs
+++ src/command.mjs
@@ -1,6 +1,6 @@
 let function_name = process.argv[2];
 let function_arguments = process.argv.slice(3);
-let result = await function_run();
+let result = await function_run(function_name, function_arguments);
 console.log({function_name, function_arguments, result})
 
 async function function_run() {
