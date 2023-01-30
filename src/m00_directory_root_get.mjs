2c5bfeb40b9b9a2dd08950b8c999827b27a3bef7
JaredMathis <jared.mathis@native.consulting>
2023-01-30 17:58:10 -0500

    whitespace

--- src/m00_directory_root_get.mjs
+++ src/m00_directory_root_get.mjs
@@ -1,5 +1,7 @@
-import { path_join } from "./path_join.mjs";
-
+import { path_join } from './path_join.mjs';
 export function m00_directory_root_get() {
-    return path_join([`./node_modules/m00/`, `./src`]);
+    return path_join([
+        `./node_modules/m00/`,
+        `./src`
+    ]);
 }
