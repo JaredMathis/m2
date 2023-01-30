eff0cad26d61b3fe6f24a9e5bb8f144a28260fe0
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:29:18 -0500

    function_imports package_scripts

--- src/package_scripts.mjs
+++ src/package_scripts.mjs
@@ -3,5 +3,7 @@ import { package_transform } from './package_transform.mjs';
 export async function package_scripts() {
     await package_transform(p => {
         property_initialize(p, 'scripts', {});
+        p.scripts.m = `node ./node_modules/m00/src/command.mjs`;
+        p.scripts.mg = `node ./node_modules/m00/src/command_git.mjs`;
     });
 }
