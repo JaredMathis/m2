a18212712358327c61a1acda2b762161645c8358
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:54:17 -0500

    function_imports package_bump

--- src/package_bump.mjs
+++ src/package_bump.mjs
@@ -1,3 +1,11 @@
-export function package_bump() {
-    
+import { package_transform } from './package_transform.mjs';
+import { error } from './error.mjs';
+export async function package_bump() {
+    await package_transform(p => {
+        if (!p.version) {
+            p.version = '0.0.0';
+        } else {
+            error('TODO');
+        }
+    });
 }
