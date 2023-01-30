0fcb01ea7acb3e909199706392ebdbfe2c81ab6f
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:28:40 -0500

    progress

--- src/list_remove.mjs
+++ src/list_remove.mjs
@@ -1,3 +1,7 @@
-export function list_remove() {
-    
+import assert from "assert.mjs";
+
+export function list_remove(list, item) {
+    let index = list.indexOf(item);
+    assert(index >= 0);
+    list.splice(index, 1);
 }
