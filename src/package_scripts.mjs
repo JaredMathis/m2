d36cec001a40c5683a8fbacdbf10f7c4e2aef29b
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:26:28 -0500

    function_imports package_scripts

--- src/package_scripts.mjs
+++ src/package_scripts.mjs
@@ -1,9 +1,7 @@
-import { package_transform } from "./package_transform.mjs";
-
+import { property_initialize } from './property_initialize.mjs';
+import { package_transform } from './package_transform.mjs';
 export async function package_scripts() {
     await package_transform(p => {
-        if (!p.scripts) {
-            p.scripts = {};
-        }
-    })
+        property_initialize(p, 'scripts', {});
+    });
 }
--- src/property_initialize.mjs
+++ src/property_initialize.mjs
@@ -1,3 +1,6 @@
-export function property_initialize() {
-    
+export function property_initialize(object, property_name, value_if_property_not_exists) {
+    if (object.hasOwnProperty(property_name)) {
+        return;
+    }
+    object[property_name] = value_if_property_not_exists;
 }
