e4816c592fb1c85deebed9e955ae580ddc8b432c
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:56:56 -0500

    function_open,for_each_async

--- src/functions_all_for_each.mjs
+++ src/functions_all_for_each.mjs
@@ -1,3 +1,10 @@
-export function functions_all_for_each() {
-    
+import { for_each } from "./for_each.mjs";
+import { functions_all_get } from "./functions_all_get.mjs";
+import { function_run } from "./function_run.mjs";
+
+export async function functions_all_for_each(for_each_function) {
+    let function_names = await functions_all_get();
+    for_each(function_names, async function_name => {
+        await function_run(for_each_function, [function_name])
+    })
 }
