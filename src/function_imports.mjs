3dffff5d1524c3f6366f5b5b31e8586c11a1c57b
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:39:09 -0500

    function_imports,function_imports

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -33,6 +33,7 @@ export async function function_imports(function_name) {
             }
         });
         console.log({
+            identifiers_existing_counts,
             identifiers_existing,
             imports_existing,
             extras,
