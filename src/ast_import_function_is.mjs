45f97415f478f6b900172bbb50d783a6ec25796c
JaredMathis <jared.mathis@native.consulting>
2023-01-29 19:20:20 -0500

    progress

--- src/ast_import_function_is.mjs
+++ src/ast_import_function_is.mjs
@@ -4,12 +4,11 @@ import { list_first } from "./list_first.mjs";
 export function ast_import_function_is(b) {
     let success = false;
     let name;
-    if (ast_node_type_is('ImportDeclaration') && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
+    if (ast_node_type_is(b, 'ImportDeclaration') && b.specifiers.length === 1 && b.source.value.startsWith('.')) {
         let first = list_first(b.specifiers);
         name = first.imported.name ;
         if (name === first.local.name) {
             success = true;
-            console.log(name)
         }
     }
     return {success, name};
--- src/ast_node_type_is.mjs
+++ src/ast_node_type_is.mjs
@@ -1,3 +1,6 @@
+import assert from "assert";
+
 export function ast_node_type_is(node, node_type) {
+    assert(arguments.length === 2);
     return node.type && node.type === node_type;
 }
