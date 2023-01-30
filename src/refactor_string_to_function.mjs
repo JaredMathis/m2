d749561dd30e4465984c6a86b9d3d6b803598c4f
JaredMathis <jared.mathis@native.consulting>
2023-01-30 18:09:04 -0500

    progress

--- src/refactor_string_to_function.mjs
+++ src/refactor_string_to_function.mjs
@@ -3,14 +3,13 @@ import { function_imports } from './function_imports.mjs';
 import { js_parse_expression } from './js_parse_expression.mjs';
 import { ast_visit } from './ast_visit.mjs';
 import { function_ast_transform } from './function_ast_transform.mjs';
-import { function_new_if_not_exists } from './function_new_if_not_exists.mjs';
 import { functions_all_get } from './functions_all_get.mjs';
 import { for_each_async } from './for_each_async.mjs';
 import { arguments_assert } from './arguments_assert.mjs';
 import { string_is } from './string_is.mjs';
 import { properties_replace } from './properties_replace.mjs';
-import { js_parse_body } from './js_parse_body.mjs';
 import { ast_node_literal_is } from './ast_node_literal_is.mjs';
+import { function_constant_string } from './function_constant_string.mjs';
 export async function refactor_string_to_function(string_value, function_name) {
     arguments_assert(arguments, string_is, string_is);
     let f = await functions_all_get();
@@ -42,12 +41,4 @@ export async function refactor_string_to_function(string_value, function_name) {
 
 
 
-async function function_constant_string(function_name, string_value) {
-    await function_new_if_not_exists(function_name);
-    await function_ast_transform(function_name, args => {
-        let { ast } = args;
-        const source_code = `export function ${function_name}() { return \`${string_value}\` }`;
-        ast.body = js_parse_body(source_code);
-    });
-}
 
