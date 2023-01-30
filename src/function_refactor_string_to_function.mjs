5066f6d1eca335467ee91af137f26440b0304fc2
JaredMathis <jared.mathis@native.consulting>
2023-01-30 18:12:02 -0500

    function_imports refactor_string_to_function

---
+++ src/function_refactor_string_to_function.mjs
@@ -0,0 +1,29 @@
+import { function_imports } from './function_imports.mjs';
+import { js_parse_expression } from './js_parse_expression.mjs';
+import { ast_visit } from './ast_visit.mjs';
+import { function_ast_transform } from './function_ast_transform.mjs';
+import { properties_replace } from './properties_replace.mjs';
+import { ast_node_literal_is } from './ast_node_literal_is.mjs';
+
+export async function function_refactor_string_to_function(fn, string_value, function_name) {
+    if (fn == function_name) {
+        return;
+    }
+    let changed = false;
+    await function_ast_transform(fn, args => {
+        let { ast } = args;
+        ast_visit(ast, v => {
+            let { node } = v;
+            if (ast_node_literal_is(node)) {
+                if (node.value === string_value) {
+                    let e = js_parse_expression(`${function_name}()`);
+                    properties_replace(node, e);
+                    changed = true;
+                }
+            }
+        });
+    });
+    if (changed) {
+        await function_imports(fn);
+    }
+}
--- src/refactor_string_to_function.mjs
+++ src/refactor_string_to_function.mjs
@@ -1,44 +1,14 @@
-import { log } from './log.mjs';
-import { function_imports } from './function_imports.mjs';
-import { js_parse_expression } from './js_parse_expression.mjs';
-import { ast_visit } from './ast_visit.mjs';
-import { function_ast_transform } from './function_ast_transform.mjs';
 import { functions_all_get } from './functions_all_get.mjs';
 import { for_each_async } from './for_each_async.mjs';
 import { arguments_assert } from './arguments_assert.mjs';
 import { string_is } from './string_is.mjs';
-import { properties_replace } from './properties_replace.mjs';
-import { ast_node_literal_is } from './ast_node_literal_is.mjs';
 import { function_constant_string } from './function_constant_string.mjs';
+import { function_refactor_string_to_function } from './function_refactor_string_to_function.mjs';
 export async function refactor_string_to_function(string_value, function_name) {
     arguments_assert(arguments, string_is, string_is);
     let f = await functions_all_get();
     await function_constant_string(function_name, string_value);
     await for_each_async(f, async fn => {
-        if (fn == function_name) {
-            return;
-        }
-        let changed = false;
-        await function_ast_transform(fn, args => {
-            let {ast} = args;
-            ast_visit(ast, v => {
-                let {node} = v;
-                if (ast_node_literal_is(node)) {
-                    if (node.value === string_value) {
-                        let e = js_parse_expression(`${ function_name }()`);
-                        properties_replace(node, e);
-                        changed = true;
-                    }
-                }
-            });
-        });
-        if (changed) {
-            await function_imports(fn);
-        }
+        await function_refactor_string_to_function(fn, string_value, function_name);
     });
 }
-
-
-
-
-
