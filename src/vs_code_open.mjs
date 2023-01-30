a33c362c24c5a33b15a81d5eb99574b17ae7f152
JaredMathis <jared.mathis@native.consulting>
2023-01-30 15:45:40 -0500

    progress

--- src/vs_code_open.mjs
+++ src/vs_code_open.mjs
@@ -1,4 +1,4 @@
 import { run } from './run.mjs';
-export function vs_code_open(path_to_file) {
-    run(`code ${ path_to_file }`);
+export async function vs_code_open(path_to_file) {
+    await run(`code ${ path_to_file }`);
 }
