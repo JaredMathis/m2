b84ea9013fbb6e65b4855cabaeb866a95603b2cf
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:25:17 -0500

    function_open property_initialize

--- src/package_scripts.mjs
+++ src/package_scripts.mjs
@@ -1,3 +1,9 @@
-export function package_scripts() {
-    
+import { package_transform } from "./package_transform.mjs";
+
+export async function package_scripts() {
+    await package_transform(p => {
+        if (!p.scripts) {
+            p.scripts = {};
+        }
+    })
 }
---
+++ src/property_initialize.mjs
@@ -0,0 +1,3 @@
+export function property_initialize() {
+    
+}
