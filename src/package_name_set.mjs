3d20e28212ca3982156aab6b763892ca004f9451
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:53:23 -0500

    refactor

--- src/package_name_set.mjs
+++ src/package_name_set.mjs
@@ -1,10 +1,9 @@
 import { merge } from './merge.mjs';
 import { package_transform } from './package_transform.mjs';
 export async function package_name_set(name) {
-    const transform = p => {
+    await package_transform(p => {
         merge(p, { name });
-    };
-    await package_transform(transform);
+    });
 }
 
 
