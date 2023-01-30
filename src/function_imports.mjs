6dcb289a258c1378d3dbe8225ce67da00b27b03d
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:06:37 -0500

    progress

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -14,6 +14,7 @@ export async function function_imports(function_name) {
         let function_name_identifiers = list_intersection(identifiers_existing, function_names);
         let without_me = list_difference(function_name_identifiers, [function_name]);
         let missing = list_difference(without_me, imports_existing);
+        console.log(missing)
     });
     if (false) {
         log({
