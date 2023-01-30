3586f9f7e10536628dde8b989c94e0ece7e534c7
JaredMathis <jared.mathis@native.consulting>
2023-01-30 16:12:44 -0500

    function_imports m00_init

--- src/m00_init.mjs
+++ src/m00_init.mjs
@@ -1,4 +1,6 @@
+import { package_scripts } from './package_scripts.mjs';
 import { gitignore_node_modules } from './gitignore_node_modules.mjs';
 export async function m00_init() {
     await gitignore_node_modules();
+    await package_scripts();
 }
