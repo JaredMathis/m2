21375b62848a4d7d98aad9f2d60056f639357fca
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:41:28 -0500

    function_imports package_name_set

--- src/package_name_set.mjs
+++ src/package_name_set.mjs
@@ -1,8 +1,10 @@
+import { merge } from './merge.mjs';
+import { file_write } from './file_write.mjs';
 import { file_json_read } from './file_json_read.mjs';
 export async function package_name_set(name) {
-    let p = await file_json_read('package.json');
-    merge(p, {name});
+    const file_path = 'package.json';
+    let p = await file_json_read(file_path);
+    merge(p, { name });
+    let json = JSON.stringify(p);
+    await file_write(file_path, json);
 }
-
-
-
