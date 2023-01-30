550666c389c5b993a1fd5ead0a27447e96188005
JaredMathis <jared.mathis@native.consulting>
2023-01-30 17:38:45 -0500

    function_imports refactor_string_to_function

--- src/refactor_string_to_function.mjs
+++ src/refactor_string_to_function.mjs
@@ -3,7 +3,10 @@ import { function_new_if_not_exists } from './function_new_if_not_exists.mjs';
 import { log } from './log.mjs';
 import { functions_all_get } from './functions_all_get.mjs';
 import { for_each_async } from './for_each_async.mjs';
+import { arguments_assert } from './arguments_assert.mjs';
+import { string_is } from './string_is.mjs';
 export async function refactor_string_to_function(string_value, function_name) {
+    arguments_assert(arguments, string_is, string_is);
     let f = await functions_all_get();
     await function_new_if_not_exists(function_name);
     await function_transform(function_name, ast => {
