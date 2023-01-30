beaf4a1a16e4e64ab497e1d095c1af31c620a45c
JaredMathis <jared.mathis@native.consulting>
2023-01-30 17:53:48 -0500

    function_imports js_parse_expression

--- src/js_parse_expression.mjs
+++ src/js_parse_expression.mjs
@@ -1,8 +1,8 @@
-import { arguments_assert } from "./arguments_assert.mjs";
-import { string_is } from "./string_is.mjs";
-
+import { js_parse } from './js_parse.mjs';
+import { arguments_assert } from './arguments_assert.mjs';
+import { string_is } from './string_is.mjs';
 export function js_parse_expression(source_code) {
-    arguments_assert(arguments, string_is)
+    arguments_assert(arguments, string_is);
     const parsed = js_parse(source_code);
     let parsed_body = parsed.body;
     let {expression} = parsed_body;
