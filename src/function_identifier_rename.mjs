a5c69155137861f5f8e675231dcd5b789502b5bf
JaredMathis <jared.mathis@native.consulting>
2023-01-29 22:28:21 -0500

    function_imports function_identifier_rename

--- src/function_identifier_rename.mjs
+++ src/function_identifier_rename.mjs
@@ -1,6 +1,17 @@
+import { ast_visit } from './ast_visit.mjs';
+import { log } from './log.mjs';
+import { ast_identifier_is } from './ast_identifier_is.mjs';
 import { function_ast_transform } from './function_ast_transform.mjs';
 export async function function_identifier_rename(function_name, identifier_before, identifier_after) {
     await function_ast_transform(function_name, async args => {
         let {ast} = args;
+        ast_visit(ast, v => {
+            let {node} = v;
+            if (ast_identifier_is(node)) {
+                if (node.name === identifier_before) {
+                    log(node);
+                }
+            }
+        });
     });
 }
