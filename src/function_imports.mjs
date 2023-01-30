4776169c79e78d044d02485f83fd738fd83feae6
JaredMathis <jared.mathis@native.consulting>
2023-01-29 19:06:31 -0500

    progress

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -3,12 +3,13 @@ import { ast_imports } from './ast_imports.mjs';
 import {function_ast_get} from './function_ast_get.mjs';
 import {log} from './log.mjs';
 import {directory_read} from './directory_read.mjs';
+import { directory_root_get } from './directory_root_get.mjs';
 export async function function_imports(function_name) {
     let ast = await function_ast_get(function_name);
     let imports_existing = ast_imports(ast);
     let identifiers_existing = ast_identifiers(ast);
 
-    directory_read()
+    directory_read(directory_root_get());
 
     log(identifiers_existing);
 }
