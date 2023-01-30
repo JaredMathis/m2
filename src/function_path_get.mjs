91082159682c1e810b1e08adb44d673291b3eee4
JaredMathis <jared.mathis@native.consulting>
2023-01-30 15:49:56 -0500

    progress

--- src/function_path_get.mjs
+++ src/function_path_get.mjs
@@ -1,8 +1,20 @@
 import { directory_root_get } from './directory_root_get.mjs';
+import { file_exists } from './file_exists.mjs';
 import { path_join } from './path_join.mjs';
+import { undefined_is } from './undefined_is.mjs';
+import { assert } from './assert.mjs';
 export async function function_path_get(function_name) {
-    return path_join([
-        directory_root_get(),
-        `${ function_name }.mjs`
-    ]);
+    let result;
+    await for_each_async(directory_root_get(), async directory_path => {
+        let file_path = path_join([
+            directory_path,
+            `${ function_name }.mjs`
+        ]);
+        if (await file_exists(file_path)) {
+            assert(undefined_is(result))
+            result = file_path;
+        }
+    })
+    assert(!undefined_is(result))
+    return result;
 }
