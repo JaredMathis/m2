fb4e1911bb58efddd8aa048e6407df141761cef9
JaredMathis <jared.mathis@native.consulting>
2023-01-29 19:21:54 -0500

    progress

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -8,11 +8,11 @@ import { function_path_to_name } from './function_path_to_name.mjs';
 import { list_intersection } from './list_intersection.mjs';
 import { list_difference } from './list_difference.mjs';
 export async function function_imports(function_name) {
+    let files = await directory_read(directory_root_get());
     let ast = await function_ast_get(function_name);
     let imports_existing = ast_imports(ast);
     let identifiers_existing = ast_identifiers(ast);
 
-    let files = await directory_read(directory_root_get());
     let function_names = files.map(f => function_path_to_name(f));
 
     let function_name_identifiers = list_intersection(identifiers_existing, function_names);
