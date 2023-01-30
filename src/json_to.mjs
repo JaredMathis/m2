46de56d971ad48b9e60ef9558b2e271a7fdb90c0
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:42:02 -0500

    refactor

---
+++ src/json_to.mjs
@@ -0,0 +1,3 @@
+export function json_to(p) {
+    return JSON.stringify(p);
+}
--- src/package_name_set.mjs
+++ src/package_name_set.mjs
@@ -1,6 +1,7 @@
 import { merge } from './merge.mjs';
 import { file_write } from './file_write.mjs';
 import { file_json_read } from './file_json_read.mjs';
+import { json_to } from './json_to.mjs';
 export async function package_name_set(name) {
     const file_path = 'package.json';
     let p = await file_json_read(file_path);
@@ -9,6 +10,4 @@ export async function package_name_set(name) {
     await file_write(file_path, json);
 }
 
-function json_to(p) {
-    return JSON.stringify(p);
-}
+
