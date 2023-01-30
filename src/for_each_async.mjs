677a9879105dead2428f58158e756982f7d461cc
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:57:48 -0500

    progress

--- src/for_each_async.mjs
+++ src/for_each_async.mjs
@@ -1,3 +1,5 @@
-export function for_each_async() {
-    
+export async function for_each_async(list, lambda) {
+    for (let l of list) {
+        await lambda(l);
+    }
 }
--- src/functions_all_for_each.mjs
+++ src/functions_all_for_each.mjs
@@ -1,10 +1,10 @@
-import { for_each } from "./for_each.mjs";
+import { for_each_async } from "./for_each_async.mjs";
 import { functions_all_get } from "./functions_all_get.mjs";
 import { function_run } from "./function_run.mjs";
 
 export async function functions_all_for_each(for_each_function) {
     let function_names = await functions_all_get();
-    for_each(function_names, async function_name => {
+    await for_each_async(function_names, async function_name => {
         await function_run(for_each_function, [function_name])
     })
 }
