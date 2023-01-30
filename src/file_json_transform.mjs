2802de8277596ac373daa76ec3f760d2b9e9a819
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:45:17 -0500

    function_imports file_json_transform

--- src/file_json_transform.mjs
+++ src/file_json_transform.mjs
@@ -1,6 +1,7 @@
+import { file_json_read } from './file_json_read.mjs';
+import { file_json_write } from './file_json_write.mjs';
 export async function file_json_transform(file_path, transform) {
     let parsed = await file_json_read(file_path);
     await transform(parsed);
     await file_json_write(file_path, parsed);
-    
 }
