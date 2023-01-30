c6e3b85f22df1a927ac02954f8a90cd25fc698d4
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:50:51 -0500

    function_imports,function_whitespace

--- src/function_whitespace.mjs
+++ src/function_whitespace.mjs
@@ -1,4 +1,5 @@
+import { noop } from './noop.mjs';
 import { function_ast_transform } from './function_ast_transform.mjs';
 export async function function_whitespace(function_name) {
-    await function_ast_transform(function_name);
+    await function_ast_transform(function_name, noop);
 }
