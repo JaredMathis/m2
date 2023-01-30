0c98895e1028cc1050c3720bbcd6ae1c833cda03
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:44:34 -0500

    refactor

--- src/file_json_transform.mjs
+++ src/file_json_transform.mjs
@@ -1,3 +1,6 @@
-export function file_json_transform() {
+export async function file_json_transform(file_path, transform) {
+    let parsed = await file_json_read(file_path);
+    await transform(parsed);
+    await file_json_write(file_path, parsed);
     
 }
