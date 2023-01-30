a4e9426317548074c12eaa0328358375295587de
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:52:10 -0500

    refactor

--- src/package_name_set.mjs
+++ src/package_name_set.mjs
@@ -1,7 +1,11 @@
 import { file_json_transform } from './file_json_transform.mjs';
 import { merge } from './merge.mjs';
 export async function package_name_set(name) {
-    await file_json_transform('package.json', p => {
+    await file_json_transform(package_path_get(), p => {
         merge(p, { name });
     });
+}
+
+function package_path_get() {
+    return 'package.json';
 }
