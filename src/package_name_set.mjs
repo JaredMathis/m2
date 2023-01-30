85e4e22e0bebe9887897aa4f598993602e69f9c0
JaredMathis <jared.mathis@native.consulting>
2023-01-30 09:38:24 -0500

    progress

--- src/package_name_set.mjs
+++ src/package_name_set.mjs
@@ -1,14 +1,8 @@
-import { file_read } from './file_read.mjs';
-import { json_from } from './json_from.mjs';
+import { file_json_read } from './file_json_read.mjs';
 export async function package_name_set(name) {
     const file_path = 'package.json';
     await file_json_read(file_path);
 }
 
 
-async function file_json_read(file_path) {
-    let json = await file_read(file_path);
-    let parsed = json_from(json);
-    return parsed;
-}
 
