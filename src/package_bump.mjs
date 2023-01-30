dcbfde996a10a9c948fbed35881e0e718346f128
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:06:41 -0500

    function_imports package_bump

--- src/package_bump.mjs
+++ src/package_bump.mjs
@@ -1,18 +1,19 @@
-import { integer_to } from './integer_to';
+import { assert } from './assert.mjs';
+import { integer_to } from './integer_to.mjs';
 import { package_transform } from './package_transform.mjs';
-import { todo } from './todo.mjs';
 export async function package_bump() {
     await package_transform(p => {
         if (!p.version) {
             p.version = '0.0.0';
         } else {
-            let parts = p.version.split('.');
+            let split_by = '.';
+            let parts = p.version.split(split_by);
             assert(parts.length === 3);
             let last = parts.pop();
             let last_number = integer_to(last);
+            last_number++;
+            parts.push(last_number);
+            p.version = parts.join(split_by);
         }
     });
 }
-
-
-
