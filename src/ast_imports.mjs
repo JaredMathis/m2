05bdc3a0041ebc55367a7b24d49be4949a820c13
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:24:46 -0500

    progress

--- src/ast_imports.mjs
+++ src/ast_imports.mjs
@@ -1,25 +1,13 @@
-import { log } from "./log.mjs";
-import { for_each } from "./for_each.mjs";
-import { ast_import_function_is } from "./ast_import_function_is.mjs";
+import { ast_imports_for_each } from "./ast_imports_for_each.mjs";
 
 export function ast_imports(ast) {
     let result = [];
     ast_imports_for_each(ast, for_each_import_function);
-    return result;
-
     function for_each_import_function(function_is) {
         result.push(function_is.name);
     }
+    return result;
 }
 
 
-function ast_imports_for_each(ast, for_each_import_function) {
-    let { body } = ast;
-    for_each(body, import_statement => {
-        let function_is = ast_import_function_is(import_statement);
-        if (function_is.success) {
-            for_each_import_function(function_is);
-        }
-    });
-}
 
---
+++ src/ast_imports_for_each.mjs
@@ -0,0 +1,12 @@
+import { for_each } from "./for_each.mjs";
+import { ast_import_function_is } from "./ast_import_function_is.mjs";
+
+export function ast_imports_for_each(ast, for_each_import_function) {
+    let { body } = ast;
+    for_each(body, import_statement => {
+        let function_is = ast_import_function_is(import_statement);
+        if (function_is.success) {
+            for_each_import_function(function_is);
+        }
+    });
+}
