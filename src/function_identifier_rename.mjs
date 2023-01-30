45347ece2285b224f21fd31324e90ae1eddf7169
JaredMathis <jared.mathis@native.consulting>
2023-01-29 22:33:54 -0500

    function_open string_is

--- src/function_identifier_rename.mjs
+++ src/function_identifier_rename.mjs
@@ -1,7 +1,14 @@
 import { ast_visit } from './ast_visit.mjs';
 import { ast_identifier_is } from './ast_identifier_is.mjs';
 import { function_ast_transform } from './function_ast_transform.mjs';
+import { for_each } from './for_each.mjs';
 export async function function_identifier_rename(function_name, identifier_before, identifier_after) {
+    let types = [string_is, string_is, string_is];
+    assert(arguments.length === types.length);
+    for_each(types, (type, index) => {
+        let argument = arguments[index];
+        assert(type(argument));
+    });
     await function_ast_transform(function_name, async args => {
         let {ast} = args;
         ast_visit(ast, v => {
