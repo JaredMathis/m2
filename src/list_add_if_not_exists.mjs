99845c91e0b7c7e8c2f72708269c644df89e663e
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:15:04 -0500

    package_publish

--- src/list_add_if_not_exists.mjs
+++ src/list_add_if_not_exists.mjs
@@ -1,6 +1,6 @@
 import { list_add } from './list_add.mjs';
 export function list_add_if_not_exists(list, node_modules) {
     if (!list.includes(node_modules)) {
-        list_add(node_modules);
+        list_add(list, node_modules);
     }
 }
