d49cba626108431255dd478ed173ba58f3b4c37a
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:32:57 -0500

    refactor

--- src/package_scripts.mjs
+++ src/package_scripts.mjs
@@ -3,8 +3,12 @@ import { package_transform } from './package_transform.mjs';
 export async function package_scripts() {
     await package_transform(p => {
         property_initialize(p, 'scripts', {});
-        let m00_directory_root = `./node_modules/m00/src/`;
+        let m00_directory_root = m00_directory_root_get();
         p.scripts.m = `node ${m00_directory_root}command.mjs`;
         p.scripts.mg = `node ${m00_directory_root}command_git.mjs`;
     });
+}
+
+function m00_directory_root_get() {
+    return `./node_modules/m00/src/`;
 }
