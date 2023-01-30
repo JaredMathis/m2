5cba7aeb63a1ae0d25114f2ef35f3c8dc213ba5c
JaredMathis <jared.mathis@native.consulting>
2023-01-30 17:45:35 -0500

    refactor_string_to_function ./src directory_source_get

--- src/refactor_string_to_function.mjs
+++ src/refactor_string_to_function.mjs
@@ -27,7 +27,9 @@ export async function refactor_string_to_function(string_value, function_name) {
                 if (ast_node_type_is(node, 'Literal')) {
                     if (node.value === string_value) {
                         console.log(fn, node);
-                        console.log(js_parse(`${ function_name }()`));
+                        const parsed = js_parse(`${ function_name }()`);
+                        let parsed_body = parsed.body;
+                        console.log(parsed_body);
                     }
                 }
             });
