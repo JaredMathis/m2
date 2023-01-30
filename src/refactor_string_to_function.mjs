908a58b6ddc9f4eb8835c5c5b9d923a63a5fb384
JaredMathis <jared.mathis@native.consulting>
2023-01-30 17:42:36 -0500

    function_imports refactor_string_to_function

--- src/refactor_string_to_function.mjs
+++ src/refactor_string_to_function.mjs
@@ -1,7 +1,8 @@
+import { ast_visit } from './ast_visit.mjs';
+import { ast_node_identifier_is } from './ast_node_identifier_is.mjs';
 import { function_ast_transform } from './function_ast_transform.mjs';
 import { js_parse } from './js_parse.mjs';
 import { function_new_if_not_exists } from './function_new_if_not_exists.mjs';
-import { log } from './log.mjs';
 import { functions_all_get } from './functions_all_get.mjs';
 import { for_each_async } from './for_each_async.mjs';
 import { arguments_assert } from './arguments_assert.mjs';
@@ -15,6 +16,16 @@ export async function refactor_string_to_function(string_value, function_name) {
         ast.body = js_parse(`export function ${ function_name }() { return \`${ string_value }\` }`).body;
     });
     await for_each_async(f, async fn => {
-        log(fn);
+        if (fn == function_name) {
+            return;
+        }
+        await function_ast_transform(fn, args => {
+            let {ast} = args;
+            ast_visit(ast, v => {
+                let {node} = v;
+                if (ast_node_identifier_is(node)) {
+                }
+            });
+        });
     });
 }
