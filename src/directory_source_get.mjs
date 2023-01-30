d7736f49dc1a03217152b2d6cb9450067bcf39ed
JaredMathis <jared.mathis@native.consulting>
2023-01-30 17:39:34 -0500

    function_imports refactor_string_to_function

---
+++ src/directory_source_get.mjs
@@ -0,0 +1,3 @@
+export function directory_source_get() {
+    
+}
--- src/refactor_string_to_function.mjs
+++ src/refactor_string_to_function.mjs
@@ -1,3 +1,4 @@
+import { function_ast_transform } from './function_ast_transform.mjs';
 import { js_parse } from './js_parse.mjs';
 import { function_new_if_not_exists } from './function_new_if_not_exists.mjs';
 import { log } from './log.mjs';
@@ -9,7 +10,8 @@ export async function refactor_string_to_function(string_value, function_name) {
     arguments_assert(arguments, string_is, string_is);
     let f = await functions_all_get();
     await function_new_if_not_exists(function_name);
-    await function_transform(function_name, ast => {
+    await function_ast_transform(function_name, args => {
+        let {ast} = args;
         ast.body = js_parse(`export function ${ function_name }() { return \`${ string_value }\` }`).body;
     });
     await for_each_async(f, async fn => {
