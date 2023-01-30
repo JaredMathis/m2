d5c6578229f76aa15ed663730537dfd1a8aa9ae6
JaredMathis <jared.mathis@native.consulting>
2023-01-29 22:30:06 -0500

    function_identifier_rename function_identifier_rename w v

--- src/function_identifier_rename.mjs
+++ src/function_identifier_rename.mjs
@@ -5,8 +5,8 @@ import { function_ast_transform } from './function_ast_transform.mjs';
 export async function function_identifier_rename(function_name, identifier_before, identifier_after) {
     await function_ast_transform(function_name, async args => {
         let {ast} = args;
-        ast_visit(ast, w => {
-            let {node} = w;
+        ast_visit(ast, v => {
+            let {node} = v;
             if (ast_identifier_is(node)) {
                 if (node.name === identifier_before) {
                     node.name = identifier_after;
