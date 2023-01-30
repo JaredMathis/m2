0c38c71460bd093c440dc79928bf80cab29c7462
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:05:21 -0500

    progress

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -8,19 +8,24 @@ import { list_intersection } from './list_intersection.mjs';
 import { list_difference } from './list_difference.mjs';
 import { function_ast_set } from './function_ast_set.mjs';
 export async function function_imports(function_name) {
-    let files = await directory_read(directory_root_get());
     let ast = await function_ast_get(function_name);
-    let imports_existing = ast_imports(ast);
-    let identifiers_existing = ast_identifiers(ast);
-    let function_names = files.map(f => function_path_to_name(f));
-    let function_name_identifiers = list_intersection(identifiers_existing, function_names);
-    let without_me = list_difference(function_name_identifiers, [function_name]);
-    let missing = list_difference(without_me, imports_existing);
+    let { imports_existing, missing } = await transform();
     await function_ast_set(function_name, ast);
     if (false) {
         log({
             imports_existing,
             missing
         });
+    }
+
+    async function transform() {
+        let files = await directory_read(directory_root_get());
+        let imports_existing = ast_imports(ast);
+        let identifiers_existing = ast_identifiers(ast);
+        let function_names = files.map(f => function_path_to_name(f));
+        let function_name_identifiers = list_intersection(identifiers_existing, function_names);
+        let without_me = list_difference(function_name_identifiers, [function_name]);
+        let missing = list_difference(without_me, imports_existing);
+        return { imports_existing, missing };
     }
 }
