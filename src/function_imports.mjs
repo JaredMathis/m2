742a146e26bdc0ec0e5364062431005e9dcc6c14
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:06:57 -0500

    function_imports,function_imports

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -5,6 +5,7 @@ import { directory_root_get } from './directory_root_get.mjs';
 import { function_path_to_name } from './function_path_to_name.mjs';
 import { list_intersection } from './list_intersection.mjs';
 import { list_difference } from './list_difference.mjs';
+import { function_ast_transform } from './function_ast_transform.mjs';
 export async function function_imports(function_name) {
     await function_ast_transform(function_name, async function transform(ast) {
         let files = await directory_read(directory_root_get());
@@ -14,10 +15,9 @@ export async function function_imports(function_name) {
         let function_name_identifiers = list_intersection(identifiers_existing, function_names);
         let without_me = list_difference(function_name_identifiers, [function_name]);
         let missing = list_difference(without_me, imports_existing);
-        console.log(missing)
+        console.log(missing);
     });
     if (false) {
-        log({
-        });
+        log({});
     }
 }
