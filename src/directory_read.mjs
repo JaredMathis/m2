72cabc2c0f7f3c46c48cad58a9753e5cf7844c40
JaredMathis <jared.mathis@native.consulting>
2023-01-30 15:55:51 -0500

    progress

--- src/directory_read.mjs
+++ src/directory_read.mjs
@@ -1,4 +1,7 @@
 import recursive from 'recursive-readdir';
 export async function directory_read(directory_path) {
-    return await recursive(directory_path);
+    console.log({directory_path});
+    let result = await recursive(directory_path);
+    console.log({result});
+    return result;
 }
--- src/directory_root_get.mjs
+++ src/directory_root_get.mjs
@@ -1,5 +1,4 @@
 import {m00_directory_root_get} from './m00_directory_root_get.mjs';
 export function directory_root_get() {
-    return [`./src`];
     return [m00_directory_root_get(), `./src`];
 }
