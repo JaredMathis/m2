6fa88dfc480eef5a1e1f9d5bfab4e594259aed33
JaredMathis <jared.mathis@native.consulting>
2023-01-30 11:08:54 -0500

    function_imports package_publish

--- src/package_publish.mjs
+++ src/package_publish.mjs
@@ -1,3 +1,6 @@
-export function package_publish() {
-    
+import { run } from './run.mjs';
+import { package_bump } from './package_bump.mjs';
+export async function package_publish() {
+    await package_bump();
+    await run(`npm publish`);
 }
