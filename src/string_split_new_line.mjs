42cd02999df42864814260387337bfe0e81a4638
JaredMathis <jared.mathis@native.consulting>
2023-01-30 10:17:43 -0500

    progress

--- src/string_split_new_line.mjs
+++ src/string_split_new_line.mjs
@@ -1,3 +1,7 @@
 export function string_split_new_line(existing) {
-    return existing.split('\n');
+    return existing.split(string_new_line());
 }
+function string_new_line() {
+    return '\n';
+}
+
