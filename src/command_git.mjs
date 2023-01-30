6c89a00af53051ccb5b0aae918e6b79c16d38c05
JaredMathis <jared.mathis@native.consulting>
2023-01-29 22:22:53 -0500

    refactor

--- src/command_git.mjs
+++ src/command_git.mjs
@@ -1,8 +1,8 @@
 import { command_run } from "./command_run.mjs";
 
 async function command_git() {
-    await command_run();
+    let result = await command_run();
     let {git_acp} = await import(`./git_acp.mjs`);
-    git_acp(process.argv.slice(2));
+    git_acp(`${result.function_name} ${result.function_arguments.join(' ')}`);
 }
 command_git();
