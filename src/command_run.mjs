42ac5ec72d3da8630ae26c1bddeb23cd99fbeece
JaredMathis <jared.mathis@native.consulting>
2023-01-29 22:21:45 -0500

    refactor

--- src/command_run.mjs
+++ src/command_run.mjs
@@ -4,10 +4,10 @@ import { log } from './log.mjs';
 export async function command_run() {
     let function_name = process.argv[2];
     let function_arguments = process.argv.slice(3);
-    let result = await function_run(function_name, function_arguments);
+    let function_result = await function_run(function_name, function_arguments);
     log({
         function_name,
         function_arguments,
-        result
+        function_result
     });
 }
