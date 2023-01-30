00cdb336bf1007250bc0693e2d9fbe5e81a82f07
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:23:47 -0500

    progress

--- src/ast_imports.mjs
+++ src/ast_imports.mjs
@@ -8,10 +8,14 @@ export function ast_imports(ast) {
     for_each(body, import_statement => {
         let function_is = ast_import_function_is(import_statement);
         if (function_is.success) {
-            result.push(function_is.name);
+            for_each_import_function(function_is);
         }
     })
     return result;
+
+    function for_each_import_function(function_is) {
+        result.push(function_is.name);
+    }
 }
 
 
