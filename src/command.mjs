97363bf8da9a8ff1a351860b325f9fc9d2d8a9f5
JaredMathis <jared.mathis@native.consulting>
2023-01-29 22:20:59 -0500

    progress

--- src/command.mjs
+++ src/command.mjs
@@ -1,13 +1,2 @@
-import { function_run } from './function_run.mjs';
-import { log } from './log.mjs';
-async function command_run() {
-    let function_name = process.argv[2];
-    let function_arguments = process.argv.slice(3);
-    let result = await function_run(function_name, function_arguments);
-    log({
-        function_name,
-        function_arguments,
-        result
-    });
-}
+import { command_run } from './command_run.mjs';
 command_run();
---
+++ src/command_run.mjs
@@ -0,0 +1,13 @@
+import { function_run } from './function_run.mjs';
+import { log } from './log.mjs';
+
+export async function command_run() {
+    let function_name = process.argv[2];
+    let function_arguments = process.argv.slice(3);
+    let result = await function_run(function_name, function_arguments);
+    log({
+        function_name,
+        function_arguments,
+        result
+    });
+}
