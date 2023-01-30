9ef34c29654d20bf89bbdc2c65e47fdcfebf4cad
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:16:29 -0500

    function_imports,function_imports

--- src/function_imports.mjs
+++ src/function_imports.mjs
@@ -1,3 +1,4 @@
+import log from './log.mjs';
 import { ast_identifiers } from './ast_identifiers.mjs';
 import { ast_imports } from './ast_imports.mjs';
 import { directory_read } from './directory_read.mjs';
@@ -6,6 +7,8 @@ import { function_path_to_name } from './function_path_to_name.mjs';
 import { list_intersection } from './list_intersection.mjs';
 import { list_difference } from './list_difference.mjs';
 import { function_ast_transform } from './function_ast_transform.mjs';
+import { js_parse } from './js_parse.mjs';
+import { for_each } from './for_each.mjs';
 export async function function_imports(function_name) {
     await function_ast_transform(function_name, async function transform(args) {
         let {ast} = args;
@@ -16,11 +19,9 @@ export async function function_imports(function_name) {
         let function_name_identifiers = list_intersection(identifiers_existing, function_names);
         let without_me = list_difference(function_name_identifiers, [function_name]);
         let missing = list_difference(without_me, imports_existing);
-        console.log({
-            missing,
-            identifiers_existing
-        });
-        console.log(ast.body);
+        let missing_imports = missing.map(m => `import ${ m } from './${ m }.mjs'`).join(';');
+        let parsed = js_parse(missing_imports);
+        ast.body = parsed.body.concat(ast.body);
     });
     if (false) {
         log({});
