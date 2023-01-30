13dbb1e4c8e7c88a1cbe7afbde3df034df2dc6a7
JaredMathis <jared.mathis@native.consulting>
2023-01-29 21:00:36 -0500

    progress

--- src/function_ast_set.mjs
+++ src/function_ast_set.mjs
@@ -1,3 +1,5 @@
-export function function_ast_set(function_name) {
-    
+import { js_unparse } from "./js_unparse.mjs";
+
+export function function_ast_set(function_name, ast) {
+    let code = js_unparse(ast);
 }
