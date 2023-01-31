caebc4bc6794b5566cf10073e8ad721ac3134d62
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:15:01 -0500

    function_auto defaults_get

--- src/defaults_get.mjs
+++ src/defaults_get.mjs
@@ -1,2 +1,8 @@
-export function defaults_get() {
+import { defaults_data } from './defaults_data.mjs';
+import { property_has } from './property_has.mjs';
+export function defaults_get(name) {
+    if (property_has(defaults_data(), name)) {
+        return defaults_data()[name];
+    }
+    return {};
 }
