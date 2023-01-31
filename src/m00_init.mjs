e92ba157edc87b0eadaadcb2068c363ee00fd437
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:53:41 -0500

    function_auto m00_init

--- src/m00_init.mjs
+++ src/m00_init.mjs
@@ -1,6 +1,9 @@
 import { package_scripts } from './package_scripts.mjs';
 import { gitignore_node_modules } from './gitignore_node_modules.mjs';
+import { directory_new } from './directory_new.mjs';
+import { directory_source_get } from './directory_source_get.mjs';
 export async function m00_init() {
     await gitignore_node_modules();
     await package_scripts();
+    await directory_new(directory_source_get());
 }
