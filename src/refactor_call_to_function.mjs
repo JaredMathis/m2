1e06ab20b67f45769a5d49b73e75aa197b982f68
JaredMathis <jared.mathis@native.consulting>
2023-01-30 19:43:47 -0500

    function_auto refactor_call_to_function

--- src/refactor_call_to_function.mjs
+++ src/refactor_call_to_function.mjs
@@ -3,23 +3,26 @@ import { ast_node_identifier_is } from './ast_node_identifier_is.mjs';
 import { ast_visit } from './ast_visit.mjs';
 import { functions_all_for_each } from './functions_all_for_each.mjs';
 import { log } from './log.mjs';
+import { function_ast_transform } from './function_ast_transform.mjs';
 export async function refactor_call_to_function(function_call_property_name, function_name) {
     await functions_all_for_each(async fn => {
-        let {ast} = args;
-        ast_visit(ast, v => {
-            let {node} = v;
-            if (ast_node_type_is(node, 'CallExpression')) {
-                let {callee} = node;
-                if (ast_node_type_is(callee, 'MemberExpression')) {
-                    let {property} = callee;
-                    if (ast_node_identifier_is(property)) {
-                        let {name} = property;
-                        if (name === function_call_property_name) {
-                            log(node);
+        function_ast_transform(fn, args => {
+            let {ast} = args;
+            ast_visit(ast, v => {
+                let {node} = v;
+                if (ast_node_type_is(node, 'CallExpression')) {
+                    let {callee} = node;
+                    if (ast_node_type_is(callee, 'MemberExpression')) {
+                        let {property} = callee;
+                        if (ast_node_identifier_is(property)) {
+                            let {name} = property;
+                            if (name === function_call_property_name) {
+                                log(node);
+                            }
                         }
                     }
                 }
-            }
+            });
         });
     });
 }
