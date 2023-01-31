6d12b642fb6ec4b1df5ef46a84d87b6b52ce9894
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:32:10 -0500

    function_auto click

--- src/click.mjs
+++ src/click.mjs
@@ -1,2 +1,7 @@
-export function click() {
+import { arguments_assert } from './arguments_assert.mjs';
+import { defined_is } from './defined_is.mjs';
+import { function_is } from './function_is.mjs';
+export function click(e, on_click) {
+    arguments_assert(arguments, defined_is, function_is);
+    e.addEventListener(on_click);
 }
