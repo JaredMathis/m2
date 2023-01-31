d1e57b17f2903acb83bfb0a4725f966eed3a175c
JaredMathis <jared.mathis@native.consulting>
2023-01-30 19:33:23 -0500

    function_auto defaults_set

--- src/defaults_set.mjs
+++ src/defaults_set.mjs
@@ -1,5 +1,5 @@
 import { property_set } from './property_set.mjs';
-let defaults = [];
+let defaults = {};
 export function defaults_set(name, default_value) {
     property_set(defaults, name, default_value);
 }
