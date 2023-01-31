605e4f04ae4f31ff4d7b838b1b4910922e76e52a
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:11:55 -0500

    function_auto element_style

--- src/element.mjs
+++ src/element.mjs
@@ -1,5 +1,6 @@
 import { element_child_add } from './element_child_add.mjs';
 export function element(parent, tag_name) {
     let e = document.createElement(tag_name);
+    element_style(e, defaults_get(element));
     element_child_add(parent, e);
 }
