714566bf33f8277ddce174d2877c3a0a60cbf4fd
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:28:13 -0500

    function_auto button

--- src/button.mjs
+++ src/button.mjs
@@ -1,3 +1,4 @@
+import { element_style_default } from './element_style_default.mjs';
 import { arguments_assert } from './arguments_assert.mjs';
 import { defined_is } from './defined_is.mjs';
 import { element } from './element.mjs';
@@ -6,5 +7,6 @@ import { string_is } from './string_is.mjs';
 export function button(parent, text, on_click) {
     arguments_assert(arguments, defined_is, string_is, function_is);
     let b = element(parent, 'button');
+    element_style_default(b, button);
     return b;
 }
