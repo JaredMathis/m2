13026e99e3c4a8d20b8fc8873108a5d2298f6c28
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:07:02 -0500

    function_auto tautology

--- src/list_add_front.mjs
+++ src/list_add_front.mjs
@@ -1,4 +1,7 @@
+import { arguments_assert } from './arguments_assert.mjs';
 import { list_concat_front } from './list_concat_front.mjs';
+import { list_is } from './list_is.mjs';
 export function list_add_front(list, item) {
+    arguments_assert(arguments, list_is, defined_is);
     list_concat_front(list, [item]);
 }
