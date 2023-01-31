035446ef04759b143cb135d212704c9c57b4470d
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:56:50 -0500

    package_publish

--- src/package_scripts.mjs
+++ src/package_scripts.mjs
@@ -1,11 +1,12 @@
 import { property_initialize } from './property_initialize.mjs';
 import { package_transform } from './package_transform.mjs';
 import { m00_directory_root_get } from './m00_directory_root_get.mjs';
+import { path_join } from './path_join.mjs';
 export async function package_scripts() {
     await package_transform(p => {
         property_initialize(p, 'scripts', {});
         let m00_directory_root = m00_directory_root_get();
-        p.scripts.m = `node ${ m00_directory_root }command.mjs`;
-        p.scripts.mg = `node ${ m00_directory_root }command_git.mjs`;
+        p.scripts.m = `node ${ path_join([m00_directory_root, `command.mjs`]) }`;
+        p.scripts.mg = `node ${ path_join([m00_directory_root, `command_git.mjs`]) }`;
     });
 }
