709c37d898ccac9a6d9ed3cf9038f65289eff4fd
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:23:29 -0500

    noop refactor

--- src/element.mjs
+++ src/element.mjs
@@ -2,8 +2,7 @@ import { element_child_add } from './element_child_add.mjs';
 import { element_style_default } from './element_style_default.mjs';
 export function element(parent, tag_name) {
     let e = document.createElement(tag_name);
-    let component = element;
-    element_style_default(e, component);
+    element_style_default(e, element);
     element_child_add(parent, e);
 }
 
