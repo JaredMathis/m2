f861049224674b1fe51baa222a4da8315295c7cd
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:36:49 -0500

    function_auto element_input

--- src/element_input.mjs
+++ src/element_input.mjs
@@ -1,2 +1,8 @@
-export function element_input() {
+import { element } from './element.mjs';
+import { element_style_default } from './element_style_default.mjs';
+export function element_input(parent, placeholder) {
+    let b = element(parent, 'input');
+    element_style_default(b, element_input);
+    b.placeholder = placeholder;
+    return b;
 }
