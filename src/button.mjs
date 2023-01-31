97446a5498373429d66fd91f4d01ad44242f3fe5
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:26:13 -0500

    function_auto button

--- src/button.mjs
+++ src/button.mjs
@@ -1,2 +1,10 @@
-export function button() {
+import { arguments_assert } from './arguments_assert.mjs';
+import { defined_is } from './defined_is.mjs';
+import { element } from './element.mjs';
+import { function_is } from './function_is.mjs';
+import { string_is } from './string_is.mjs';
+export function button(parent, text, on_click) {
+    arguments_assert(arguments, defined_is, string_is, function_is);
+    let b = element(parent, 'button');
+    return b;
 }
