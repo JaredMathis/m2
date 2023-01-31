163b54d3cb621e0569154cc0af98e577d961e0d9
JaredMathis <jared.mathis@native.consulting>
2023-01-30 19:53:37 -0500

    function_auto list_add_front

--- src/list_add_front.mjs
+++ src/list_add_front.mjs
@@ -1,2 +1,5 @@
-export function list_add_front() {
+import { element } from './element.mjs';
+import { list_concat_front } from './list_concat_front.mjs';
+export function list_add_front(list, element) {
+    list_concat_front(list, [element]);
 }
