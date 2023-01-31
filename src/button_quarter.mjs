8ab065a9c07be1f0d41d75044a7f462742597c38
JaredMathis <jared.mathis@native.consulting>
2023-01-30 20:45:42 -0500

    function_auto button_quarter

--- src/button_quarter.mjs
+++ src/button_quarter.mjs
@@ -1,2 +1,10 @@
-export function button_quarter() {
+import { arguments_assert } from './arguments_assert.mjs';
+import { defined_is } from './defined_is.mjs';
+import { string_is } from './string_is.mjs';
+import { function_is } from './function_is.mjs';
+import { button } from './button.mjs';
+export function button_quarter(parent, text, on_click) {
+    arguments_assert(arguments, defined_is, string_is, function_is);
+    let b = button(parent, text, on_click);
+    return b;
 }
