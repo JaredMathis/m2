5871ef00114daa5678dbd16ddded1c506a3c3ee1
JaredMathis <jared.mathis@native.consulting>
2023-01-30 19:39:53 -0500

    function_auto refactor_call_to_function

--- src/refactor_call_to_function.mjs
+++ src/refactor_call_to_function.mjs
@@ -1,12 +1,16 @@
+import { ast_node_type_is } from './ast_node_type_is.mjs';
+import { ast_node_identifier_is } from './ast_node_identifier_is.mjs';
 import { ast_visit } from './ast_visit.mjs';
-import { ast_node_literal_is } from './ast_node_literal_is.mjs';
 import { functions_all_for_each } from './functions_all_for_each.mjs';
 export async function refactor_call_to_function(function_call_property_name, function_name) {
     await functions_all_for_each(async fn => {
         let {ast} = args;
         ast_visit(ast, v => {
             let {node} = v;
-            if (ast_node_literal_is(node)) {
+            if (ast_node_type_is(node, 'CallExpression')) {
+                let {callee} = node;
+                if (ast_node_identifier_is(callee)) {
+                }
             }
         });
     });
