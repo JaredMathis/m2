dc9bae9e270417195dc705bc256ecffdf3b9a0fc
JaredMathis <jared.mathis@native.consulting>
2023-01-30 19:37:07 -0500

    function_auto refactor_call_to_function

--- src/refactor_call_to_function.mjs
+++ src/refactor_call_to_function.mjs
@@ -1,2 +1,5 @@
-export function refactor_call_to_function() {
+import { functions_all_for_each } from './functions_all_for_each.mjs';
+export async function refactor_call_to_function(function_call_property_name, function_name) {
+    await functions_all_for_each(async fn => {
+    });
 }
