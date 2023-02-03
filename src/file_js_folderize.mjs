501333984c66a0d76a142c56b0054afc9db2de9e
JaredMathis <jared.mathis@native.consulting>
2023-02-03 18:55:38 -0500

    file_js_folderize src/ast_visit.mjs

--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -4,6 +4,8 @@ import { ast_node_type_is } from './ast_node_type_is.mjs';
 import { for_each } from './for_each.mjs';
 import { js_parse } from './js_parse.mjs';
 import { file_read } from './file_read.mjs';
+import { function_path_get } from './function_path_get.mjs';
+import { directory_source_get } from './directory_source_get.mjs';
 export async function file_js_folderize(file_path) {
     let exports = [];
     let text = await file_read(file_path);
@@ -23,5 +25,6 @@ export async function file_js_folderize(file_path) {
     }
     let {id} = declaration;
     let function_name = id.name;
-    log(function_name);
+    let function_path_new = function_path_get(directory_source_get(), function_name);
+    log(function_path_new);
 }
