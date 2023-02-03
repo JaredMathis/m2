e611782961a2650ceb90dc437b1384917edf5daf
JaredMathis <jared.mathis@native.consulting>
2023-02-03 18:46:42 -0500

    function_imports file_js_folderize

--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -1,14 +1,18 @@
+import { list_first } from './list_first.mjs';
 import { ast_node_type_is } from './ast_node_type_is.mjs';
 import { for_each } from './for_each.mjs';
 import { js_parse } from './js_parse.mjs';
-import { log } from './log.mjs';
 import { file_read } from './file_read.mjs';
 export async function file_js_folderize(file_path) {
+    let exports = [];
     let text = await file_read(file_path);
     let ast = js_parse(text);
     for_each(ast.body, b => {
         if (ast_node_type_is(b, 'ExportNamedDeclaration')) {
-            console.log(b);
+            exports.push(b);
         }
     });
+    if (exports.length === 1) {
+        let first = list_first(exports);
+    }
 }
