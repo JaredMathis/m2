3a2cd8dad48fde8e23a0ee7d8a7c3b4f743fc2aa
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:51:46 -0500

    file_js_folderize src\command_git.mjs src

--- src/file/js/folderize/imports.mjs
+++ src/file/js/folderize/imports.mjs
@@ -3,6 +3,5 @@ import { functions_all_for_each } from './../../../functions/all/for/each.mjs';
 import { function_imports } from './../../../function/imports.mjs';
 export async function file_js_folderize_imports(file_path, output_path) {
     let function_path_new = await file_js_folderize(file_path, output_path);
-    console.log({file_path, function_path_new})
     await functions_all_for_each(fn => function_imports(fn));
 }
