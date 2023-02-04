4a7cdf973b5cd14f08a4ded7e222481270b27f46
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:54:43 -0500

    function_imports file_js_folderize

--- src/file/js/folderize.mjs
+++ src/file/js/folderize.mjs
@@ -1,11 +1,11 @@
-import { log } from './log.mjs';
-import { list_first } from './list_first.mjs';
-import { ast_node_type_is } from './ast_node_type_is.mjs';
-import { for_each } from './for_each.mjs';
-import { js_parse } from './js_parse.mjs';
-import { file_read } from './file_read.mjs';
-import { function_path_get } from './function_path_get.mjs';
-import { file_rename } from './file/rename.mjs';
+import { log } from './..\\..\\..\\log.mjs';
+import { list_first } from './..\\..\\..\\list_first.mjs';
+import { ast_node_type_is } from './..\\..\\..\\ast_node_type_is.mjs';
+import { for_each } from './..\\..\\..\\for_each.mjs';
+import { js_parse } from './..\\..\\..\\js_parse.mjs';
+import { file_read } from './..\\..\\..\\file_read.mjs';
+import { function_path_get } from './..\\..\\..\\function_path_get.mjs';
+import { file_rename } from './..\\..\\rename.mjs';
 export async function file_js_folderize(file_path, output_path) {
     let exports = [];
     let ast;
