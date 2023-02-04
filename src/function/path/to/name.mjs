caae6fa1eb285d416659ac60c483bb1b04b32172
JaredMathis <jared.mathis@native.consulting>
2023-02-03 22:27:48 -0500

    noop changes

--- src/function/path/to/name.mjs
+++ src/function/path/to/name.mjs
@@ -1,5 +1,7 @@
 import path from 'path';
-import { directory_source_get } from './../../../directory/source/get.mjs';
-export function function_path_to_name(function_path) {
-    return path.relative(directory_source_get(), function_path).replaceAll('/', '_').replaceAll('\\', '_').replace('.mjs', '');
+import { arguments_assert } from '../../../arguments/assert.mjs';
+import { string_is } from '../../../string/is.mjs';
+export function function_path_to_name(function_path, directory_root) {
+    arguments_assert(arguments, string_is, string_is);
+    return path.relative(directory_root, function_path).replaceAll('/', '_').replaceAll('\\', '_').replace('.mjs', '');
 }
