e0a4fb6e4dcf3e9c8b7835a5495b17a17c8d52e4
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:05:22 -0500

    noop progress

--- src/file_write.mjs
+++ src/file_write.mjs
@@ -1,4 +1,7 @@
 import fs from 'fs';
+import path from 'path';
 export async function file_write(file_path, file_contents) {
+    let directory_path = path.dirname(file_path);
+    await fs.promises.mkdir(directory_path, { recursive: true })
     await fs.promises.writeFile(file_path, file_contents, 'utf8');
 }
