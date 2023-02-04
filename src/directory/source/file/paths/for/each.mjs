b13de8db69ea9370c9dee2612b240a81dbc7b5ba
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:20:00 -0500

    function_imports directory_source_file_paths_for_each

--- src/directory/source/file/paths/for/each.mjs
+++ src/directory/source/file/paths/for/each.mjs
@@ -1,3 +1,6 @@
-export function directory_source_file_paths_for_each() {
-    
+import { directory_source_file_paths_get } from './../get.mjs';
+import { for_each_async } from './../../../../../for/each/async.mjs';
+export async function directory_source_file_paths_for_each(file_path_for_each) {
+    let file_paths = await directory_source_file_paths_get();
+    await for_each_async(file_paths, file_path_for_each);
 }
