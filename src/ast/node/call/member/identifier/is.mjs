6704a7fbdb0fff819d8072791bffc3a5a6f92353
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:40:15 -0500

    function_auto refactor_call_to_function

--- src/ast/node/call/member/identifier/is.mjs
+++ src/ast/node/call/member/identifier/is.mjs
@@ -1,2 +1,12 @@
-export function ast_node_call_member_identifier_is() {
+export function ast_node_call_member_identifier_is(node, if_true) {
+    if (ast_node_type_is(node, 'CallExpression')) {
+        let {callee} = node;
+        if (ast_node_type_is(callee, 'MemberExpression')) {
+            let {property} = callee;
+            if (ast_node_identifier_is(property)) {
+                let {name} = property;
+                if_true(name, node, callee);
+            }
+        }
+    }
 }
--- src/refactor/call/to/function.mjs
+++ src/refactor/call/to/function.mjs
@@ -2,9 +2,8 @@ import { function_ast_transform_visit } from './../../../function/ast/transform/
 import { function_imports } from './../../../function/imports.mjs';
 import { js_parse_expression } from './../../../js/parse/expression.mjs';
 import { list_add_front } from './../../../list/add/front.mjs';
-import { ast_node_type_is } from './../../../ast/node/type/is.mjs';
-import { ast_node_identifier_is } from './../../../ast/node/identifier/is.mjs';
 import { functions_all_for_each } from './../../../functions/all/for/each.mjs';
+import { ast_node_call_member_identifier_is } from './../../../ast/node/call/member/identifier/is.mjs';
 export async function refactor_call_to_function(function_call_property_name, function_name) {
     await functions_all_for_each(async fn => {
         if (fn === function_name) {
@@ -13,22 +12,15 @@ export async function refactor_call_to_function(function_call_property_name, fun
         let changed = false;
         await function_ast_transform_visit(fn, v => {
             let {node} = v;
-            if (ast_node_type_is(node, 'CallExpression')) {
-                let {callee} = node;
-                if (ast_node_type_is(callee, 'MemberExpression')) {
-                    let {property} = callee;
-                    if (ast_node_identifier_is(property)) {
-                        let {name} = property;
-                        if (name === function_call_property_name) {
-                            let arguments_ = node.arguments;
-                            let {object} = callee;
-                            list_add_front(arguments_, object);
-                            node.callee = js_parse_expression(function_name);
-                            changed = true;
-                        }
-                    }
+            ast_node_call_member_identifier_is(node, function if_true(name, node, callee) {
+                if (name === function_call_property_name) {
+                    let arguments_ = node.arguments;
+                    let {object} = callee;
+                    list_add_front(arguments_, object);
+                    node.callee = js_parse_expression(function_name);
+                    changed = true;
                 }
-            }
+            });
         });
         if (changed) {
             await function_imports(fn);
