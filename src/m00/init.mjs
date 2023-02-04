b00bee54a80cbc271f5e5650b6bf9cad4350d88d
JaredMathis <jared.mathis@native.consulting>
2023-02-03 22:21:38 -0500

    noop changes

--- src/m00/init.mjs
+++ src/m00/init.mjs
@@ -1,14 +1,14 @@
 import { run } from './../run.mjs';
 import { package_scripts } from './../package/scripts.mjs';
 import { gitignore_node_modules } from './../gitignore/node/modules.mjs';
-import { directory_new } from './../directory/new.mjs';
 import { directory_source_get } from './../directory/source/get.mjs';
 import { index_new } from './../index/new.mjs';
+import { file_directory_create_if_not_exists } from '../file/directory/create/if/not/exists.mjs';
 export async function m00_init() {
     await gitignore_node_modules();
     await package_scripts();
     await run(`npm install webpack webpack-cli --save-dev`);
-    await directory_new(directory_source_get());
-    await directory_new('public');
+    await file_directory_create_if_not_exists(directory_source_get());
+    await file_directory_create_if_not_exists('public');
     await index_new();
 }
