660ad6a3fc5b3288a38e2c2371dc019ccd70b85a
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:33:00 -0500

    function_auto function_ast_transform_visit

--- src/function/ast/transform/visit.mjs
+++ src/function/ast/transform/visit.mjs
@@ -1,8 +1,11 @@
-export function function_ast_transform_visit(fn, for_each) {
+import { for_each } from './../../../for/each.mjs';
+import { function_ast_transform } from './../transform.mjs';
+import { ast_visit } from './../../../ast/visit.mjs';
+export async function function_ast_transform_visit(fn, for_each) {
     await function_ast_transform(fn, args => {
         let {ast} = args;
         ast_visit(ast, v => {
-            for_each(v)
+            for_each(v);
         });
-})
+    });
 }
