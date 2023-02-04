9d752ca4c6b83c364806fa72f1ef4e098b1a9453
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:27:35 -0500

    function_imports file_js_all_complexity

--- src/file/js/all/complexity.mjs
+++ src/file/js/all/complexity.mjs
@@ -1,3 +1,16 @@
-export function file_js_all_complexity() {
-    
+import { directory_source_file_paths_for_each } from './../../../directory/source/file/paths/for/each.mjs';
+import { log } from './../../../log.mjs';
+import { list_add } from './../../../list/add.mjs';
+import { file_read } from './../../read.mjs';
+export async function file_js_all_complexity() {
+    let result = [];
+    await directory_source_file_paths_for_each(async function file_path_for_each(file_path) {
+        let contents = await file_read(file_path);
+        let size = contents.length;
+        list_add(result, {
+            file_path,
+            size
+        });
+    });
+    log(result);
 }
