8c23d02ec23f0264bdc2fe95245fb20792b1c35a
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:30:13 -0500

    noop progress

--- src/file/rename.mjs
+++ src/file/rename.mjs
@@ -1,10 +1,12 @@
+import fs from 'fs';
+import path from 'path';
 import { file_exists } from "../file_exists.mjs";
 import { error } from "../error.mjs";
 import fs from 'fs';
 import {file_directory_create_if_not_exists} from './directory/create/if/not/exists.mjs';
 export async function file_rename(file_path_from, file_path_to) {
-    if (file_path_from == file_path_to) {
-        error('Destination must differ from source')
+    if (path.resolve(file_path_from) == path.resolve(file_path_to)) {
+        return;
     }
     if (await file_exists(file_path_to)) {
         error('File already exists: ' + file_path_to)
--- src/file_js_folderize.mjs
+++ src/file_js_folderize.mjs
@@ -32,5 +32,6 @@ export async function file_js_folderize(file_path, output_path) {
     let {id} = declaration;
     let function_name = id.name;
     let function_path_new = function_path_get(output_path, function_name);
+    
     await file_rename(file_path, function_path_new)
 }
