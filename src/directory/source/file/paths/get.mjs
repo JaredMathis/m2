08e915b42ab0a33f613182a589d300b1d6c12c2e
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:18:18 -0500

    function_imports directory_source_file_paths_get

--- src/directory/source/file/paths/get.mjs
+++ src/directory/source/file/paths/get.mjs
@@ -1,3 +1,7 @@
-export function directory_source_file_paths_get() {
-    
+import { directory_source_get } from './../../get.mjs';
+import { directory_read } from './../../../read.mjs';
+export async function directory_source_file_paths_get() {
+    let directory_source = directory_source_get();
+    let file_paths = await directory_read(directory_source);
+    return file_paths;
 }
