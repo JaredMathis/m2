d66c3065804371a2fb0c9700145693e4292b6c60
JaredMathis <jared.mathis@native.consulting>
2023-02-03 22:21:10 -0500

    noop changes

--- src/function/run.mjs
+++ src/function/run.mjs
@@ -1,7 +1,9 @@
 import { function_path_find } from './path/find.mjs';
+import path from 'path';
 export async function function_run(function_name, function_arguments) {
     let function_path = await function_path_find(function_name);
-    let imported = await import('../../' + function_path);
+    function_path = path.resolve(function_path);
+    let imported = await import("file://" + function_path);
     let _function = imported[function_name];
     let result = await _function(...function_arguments);
     return result;
