1f916f3d34bd199e23b29eb428e920c55ec615ff
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:18:39 -0500

    function_imports refactor_folderize

--- src/refactor/folderize.mjs
+++ src/refactor/folderize.mjs
@@ -1,10 +1,8 @@
+import { directory_source_file_paths_get } from './../directory/source/file/paths/get.mjs';
 import { file_js_folderize_imports } from './../file/js/folderize/imports.mjs';
 import { for_each_async } from './../for/each/async.mjs';
-import { directory_read } from './../directory/read.mjs';
-import { directory_source_get } from './../directory/source/get.mjs';
 export async function refactor_folderize() {
-    let directory_source = directory_source_get();
-    let file_paths = await directory_read(directory_source);
+    let file_paths = await directory_source_file_paths_get();
     await for_each_async(file_paths, directory_source_for_each);
     async function directory_source_for_each(file_path) {
         await file_js_folderize_imports(file_path, directory_source);
