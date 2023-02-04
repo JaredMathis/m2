ba12efdadfa1850e8516c6d0429695f3ceeb0a22
JaredMathis <jared.mathis@native.consulting>
2023-02-03 22:38:54 -0500

    package_publish

--- src/element.mjs
+++ src/element.mjs
@@ -8,4 +8,5 @@ export function element(parent, tag_name) {
     let e = document.createElement(tag_name);
     element_style_default(e, element);
     element_child_add(parent, e);
+    return e;
 }
