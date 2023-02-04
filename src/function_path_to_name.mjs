cdc99207e6e0d25484a82572a2c60c2edde06f73
JaredMathis <jared.mathis@native.consulting>
2023-02-03 20:17:59 -0500

    noop progress

--- src/function_path_to_name.mjs
+++ src/function_path_to_name.mjs
@@ -1,4 +1,5 @@
 import path from 'path';
+import { directory_source_get } from './directory_source_get.mjs';
 export function function_path_to_name(function_path) {
-    return path.relative('src', function_path).replaceAll('/', '_').replace('.mjs', '');
+    return path.relative(directory_source_get(), function_path).replaceAll('/', '_').replace('.mjs', '');
 }
