189596e4c82e2fe1409db1f9debb6e69a772f86a
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:06:21 -0500

    noop progress

--- src/file_write.mjs
+++ src/file_write.mjs
@@ -1,7 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import { file_exists } from './file_exists.mjs';
 export async function file_write(file_path, file_contents) {
     let directory_path = path.dirname(file_path);
-    await fs.promises.mkdir(directory_path, { recursive: true })
+    if (!await file_exists(directory_path)) {
+        await fs.promises.mkdir(directory_path, { recursive: true })
+    }
     await fs.promises.writeFile(file_path, file_contents, 'utf8');
 }
