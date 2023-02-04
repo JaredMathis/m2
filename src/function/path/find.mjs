cdd384998828f91d8b7244c20ff36c75b0dc8e75
JaredMathis <jared.mathis@native.consulting>
2023-02-03 21:03:57 -0500

    noop changes

--- src/function/path/find.mjs
+++ src/function/path/find.mjs
@@ -12,16 +12,10 @@ export async function function_path_find(function_name) {
     let result;
     await for_each_async(directory_root_get(), async directory_path => {
         let file_path = function_path_get(directory_path, function_name);
-        let file_paths = [file_path];
-        if (file_path.includes('_')) {
-            file_paths.push(file_path.replaceAll('_', '/'));
+        if (await file_exists(file_path)) {
+            assert(undefined_is(result));
+            result = file_path;
         }
-        await for_each_async(file_paths, async file_path => {
-            if (await file_exists(file_path)) {
-                assert(undefined_is(result));
-                result = file_path;
-            }
-        });
     });
     if (undefined_is(result)) {
         error('Could not find function ' + function_name);
