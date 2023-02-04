320265fb976201d2c75e5c7841da5a99b268cf73
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:13:04 -0500

    noop progress

--- src/file/directory/create/if/not/exists.mjs
+++ src/file/directory/create/if/not/exists.mjs
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 import { file_exists } from '../../../../../file_exists.mjs';
 export async function file_directory_create_if_not_exists(file_path) {
     let directory_path = path.dirname(file_path);
