31b08965ed06c8db31655d36ac09f51379cef20f
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:11:55 -0500

    noop progress

--- src/file/directory/create/if/not/exists.mjs
+++ src/file/directory/create/if/not/exists.mjs
@@ -1,3 +1,8 @@
-export function file_directory_create_if_not_exists() {
-    
+import path from 'path';
+import { file_exists } from '../../../../../file_exists.mjs';
+export async function file_directory_create_if_not_exists(file_path) {
+    let directory_path = path.dirname(file_path);
+    if (!await file_exists(directory_path)) {
+        await fs.promises.mkdir(directory_path, { recursive: true });
+    }
 }
--- src/file_write.mjs
+++ src/file_write.mjs
@@ -1,14 +1,7 @@
 import fs from 'fs';
-import path from 'path';
-import { file_exists } from './file_exists.mjs';
+import {file_directory_create_if_not_exists} from './file/directory/create/if/not/exists.mjs';
 export async function file_write(file_path, file_contents) {
     await file_directory_create_if_not_exists(file_path);
     await fs.promises.writeFile(file_path, file_contents, 'utf8');
 }
 
-async function file_directory_create_if_not_exists(file_path) {
-    let directory_path = path.dirname(file_path);
-    if (!await file_exists(directory_path)) {
-        await fs.promises.mkdir(directory_path, { recursive: true });
-    }
-}
