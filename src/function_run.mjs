415b1713448658e31da3fce5d045b555bd07ecd0
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:16:34 -0500

    noop progress

--- src/function_run.mjs
+++ src/function_run.mjs
@@ -1,5 +1,8 @@
+import { function_path_find } from "./function_path_find.mjs";
+
 export async function function_run(function_name, function_arguments) {
-    let imported = await import(`./${ function_name }.mjs`);
+    let function_path = await function_path_find(function_name);
+    let imported = await import('../' + function_path);
     let _function = imported[function_name];
     let result = await _function(...function_arguments);
     return result;
--- src/refactor/folderize.mjs
+++ src/refactor/folderize.mjs
@@ -1,3 +1,7 @@
+import { directory_root_get } from "../directory_root_get.mjs";
+
 export function refactor_folderize() {
-    
+    let root_directory = directory_root_get();
+    let file_paths = await directory_read(directory_path);
+    console.log(file_paths)
 }
