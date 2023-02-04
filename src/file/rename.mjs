d9624997e063e99cadc0f16657d05d25f8d836f2
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:08:16 -0500

    noop progress

--- src/file/rename.mjs
+++ src/file/rename.mjs
@@ -1,3 +1,8 @@
+import { file_exists } from "../file_exists.mjs";
+import { error } from "../error.mjs";
+
 export function file_rename(file_path_from, file_path_to) {
-    
+    if (await file_exists(file_path_to)) {
+        error('File already exists: ' + file_path_to)
+    }
 }
