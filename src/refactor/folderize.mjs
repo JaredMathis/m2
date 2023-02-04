9c2571acf9def731de86c7418bf32b37ea2bc360
JaredMathis <jared.mathis@native.consulting>
2023-02-03 19:19:03 -0500

    noop progress

--- src/refactor/folderize.mjs
+++ src/refactor/folderize.mjs
@@ -1,8 +1,12 @@
 import { directory_read } from "../directory_read.mjs";
 import { directory_source_get } from "../directory_source_get.mjs";
+import { file_js_folderize } from "../file_js_folderize.mjs";
+import { for_each } from "../for_each.mjs";
 
 export async function refactor_folderize() {
-    let root_directory = directory_source_get();
-    let file_paths = await directory_read(root_directory);
-    console.log(root_directory, file_paths)
+    let directory_source = directory_source_get();
+    let file_paths = await directory_read(directory_source);
+    for_each(file_paths, async file_path => {
+        file_js_folderize(file_path, directory_source);
+    })
 }
