1cdeb52ed9a635270a5ce764646af32ef8953981
JaredMathis <jared.mathis@native.consulting>
2023-02-03 22:36:41 -0500

    function_auto element_with_text

--- src/element/with/text.mjs
+++ src/element/with/text.mjs
@@ -1,3 +1,7 @@
-export function element_with_text() {
-    
+import { element_text } from './../text.mjs';
+import { element } from './../../element.mjs';
+export function element_with_text(parent, tag_name, text) {
+    let e = element(parent, tag_name);
+    element_text(e, text);
+    return e;
 }
