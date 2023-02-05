b5d441e9cbb454e88f4f7eb9d54105dd1dc078cf
JaredMathis <jared.mathis@native.consulting>
2023-02-05 00:29:05 -0500

    function_open package_publish_vs_code_open

--- src/package/publish/vs/code/open.mjs
+++ src/package/publish/vs/code/open.mjs
@@ -1,3 +1,8 @@
-export function package_publish_vs_code_open() {
-    
+import { arguments_assert } from "../../../../arguments/assert.mjs";
+import { list_is } from "../../../../list/is.mjs";
+
+export async function package_publish_vs_code_open(path_to_directory) {
+    arguments_assert(arguments, list_is);
+    await package_publish();
+    await vs_code_open(path_to_directory);
 }
