b792ec48176ab2fdf99d960fa385a4947200ceb4
JaredMathis <jared.mathis@native.consulting>
2023-02-05 00:26:59 -0500

    function_auto list_min

--- src/list/min.mjs
+++ src/list/min.mjs
@@ -1,2 +1,14 @@
-export function list_min() {
+import { list_first } from './first.mjs';
+import { arguments_assert } from './../arguments/assert.mjs';
+import { for_each } from './../for/each.mjs';
+import { list_is } from './is.mjs';
+export function list_min(list) {
+    arguments_assert(arguments, list_is);
+    let min = list_first(list);
+    for_each(list, value => {
+        if (value < min) {
+            min = value;
+        }
+    });
+    return min;
 }
