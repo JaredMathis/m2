6b6acf9656edb399d56ce5350f287bfc759acbee
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:28:22 -0500

    function_auto list_slice_begin_from

--- src/list/slice/begin/from.mjs
+++ src/list/slice/begin/from.mjs
@@ -1,2 +1,8 @@
-export function list_slice_begin_from() {
+import { list_slice } from './../../slice.mjs';
+import { arguments_assert } from './../../../arguments/assert.mjs';
+import { number_negative_not } from './../../../number/negative/not.mjs';
+import { list_is } from './../../is.mjs';
+export function list_slice_begin_from(list, index) {
+    arguments_assert(arguments, list_is, number_negative_not);
+    return list_slice(0, index);
 }
