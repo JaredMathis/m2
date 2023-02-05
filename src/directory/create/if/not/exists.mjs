03dd8dd0d77a5729ff14d627b58ab5c903ec8781
JaredMathis <jared.mathis@native.consulting>
2023-02-04 19:46:19 -0500

    package_publish

--- src/directory/create/if/not/exists.mjs
+++ src/directory/create/if/not/exists.mjs
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { file_exists } from './../../../../file/exists.mjs';
 export async function directory_create_if_not_exists(directory_path) {
     if (!await file_exists(directory_path)) {
