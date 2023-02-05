0a2f7c7d03127a2f466dfac2a930d7a66f5d3133
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:25:31 -0500

    function_auto list_slice

--- src/list/slice.mjs
+++ src/list/slice.mjs
@@ -1,2 +1,6 @@
-export function list_slice() {
+import { arguments_assert } from './../arguments/assert.mjs';
+import { number_negative_not } from './../number/negative/not.mjs';
+export function list_slice(list, from, to) {
+    arguments_assert(arguments, number_negative_not, number_negative_not);
+    return list.slice(from, to);
 }
