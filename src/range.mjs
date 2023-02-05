8a0f32faff7193e6e21084e3342b75776e2b1e17
JaredMathis <jared.mathis@native.consulting>
2023-02-04 19:58:47 -0500

    function_auto for_each

--- src/range.mjs
+++ src/range.mjs
@@ -1,2 +1,6 @@
-export function range() {
+import { arguments_assert } from "./arguments/assert.mjs";
+import { number_at_least } from "./number/at/least.mjs";
+
+export function range(n) {
+    arguments_assert(arguments, number_at_least(0));
 }
