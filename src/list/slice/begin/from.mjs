2f7a506081a603305691a52cfbd6bd6e6f6cfe13
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:28:36 -0500

    function_auto list_slice_begin_from

--- src/list/slice/begin/from.mjs
+++ src/list/slice/begin/from.mjs
@@ -4,5 +4,5 @@ import { number_negative_not } from './../../../number/negative/not.mjs';
 import { list_is } from './../../is.mjs';
 export function list_slice_begin_from(list, index) {
     arguments_assert(arguments, list_is, number_negative_not);
-    return list_slice(0, index);
+    return list_slice(list, 0, index);
 }
