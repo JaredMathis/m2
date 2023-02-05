d68cd92e3fa00bcb0918bafffd4c815a5d6ad6f5
JaredMathis <jared.mathis@native.consulting>
2023-02-04 20:00:06 -0500

    function_auto range

--- src/range.mjs
+++ src/range.mjs
@@ -1,8 +1,7 @@
-import { arguments_assert } from "./arguments/assert.mjs";
-import { number_at_least } from "./number/at/least.mjs";
-
+import { arguments_assert } from './arguments/assert.mjs';
+import { number_negative_not } from './number/negative/not.mjs';
 export function range(n) {
-    arguments_assert(arguments, number_at_least(0));
+    arguments_assert(arguments, number_negative_not);
     let result = [];
     for (let i = 0; i < n; i++) {
         result.push(i);
