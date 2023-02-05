a0430f5ecc38d6587b0f05e5aaa7f799ce39f031
JaredMathis <jared.mathis@native.consulting>
2023-02-04 19:56:10 -0500

    function_auto list_shuffle

--- src/list/shuffle.mjs
+++ src/list/shuffle.mjs
@@ -1,21 +1,15 @@
-import { arguments_assert } from "../arguments/assert.mjs";
-
+import { list_is } from './is.mjs';
+import { arguments_assert } from './../arguments/assert.mjs';
 export function list_shuffle(array) {
     arguments_assert(arguments, list_is);
-    
-    let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle.
+    let currentIndex = array.length, randomIndex;
     while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex]
+        ];
     }
-  
     return array;
 }
