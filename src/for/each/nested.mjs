d13ef2b609e15acc7f92ca43fa6d7402f79656d9
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:56:08 -0500

    function_auto for_each_nested

--- src/for/each/nested.mjs
+++ src/for/each/nested.mjs
@@ -1,2 +1,8 @@
-export function for_each_nested() {
+import { for_each } from './../each.mjs';
+export function for_each_nested(list, for_each_pair) {
+    for_each(list, a => {
+        for_each(list, b => {
+            for_each_pair(a, b);
+        });
+    });
 }
