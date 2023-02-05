4ee4ba00cac11064b459f984d84179deabdea407
JaredMathis <jared.mathis@native.consulting>
2023-02-05 04:23:24 -0500

    refactor_string_to_function   string_space

--- src/commandgit.mjs
+++ src/commandgit.mjs
@@ -1,7 +1,9 @@
+import { git_acp } from './git/acp.mjs';
+import { string_space } from './string/space.mjs';
 import { command_run } from './command/run.mjs';
 async function commandgit() {
     let result = await command_run();
     let {git_acp} = await import(`./git/acp.mjs`);
-    git_acp(`${ result.function_name } ${ result.function_arguments.join(' ') }`);
+    git_acp(`${ result.function_name } ${ result.function_arguments.join(string_space()) }`);
 }
 commandgit();
--- src/string/split/space.mjs
+++ src/string/split/space.mjs
@@ -1,4 +1,5 @@
+import { string_space } from './../space.mjs';
 import { string_split } from './../split.mjs';
 export function string_split_space(s) {
-    return string_split(s, ' ');
+    return string_split(s, string_space());
 }
