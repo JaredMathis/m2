c732b2e1c4662beb25348d192207a714b2192a1b
JaredMathis <jared.mathis@native.consulting>
2023-02-05 04:28:31 -0500

    function_auto function_auto_run

--- src/function/auto/run.mjs
+++ src/function/auto/run.mjs
@@ -1,4 +1,7 @@
+import { function_run } from './../run.mjs';
 import { function_auto } from './../auto.mjs';
 export async function function_auto_run(function_name) {
+    let arguments_ = Array.from(arguments);
     await function_auto(function_name);
+    await function_run(arguments_.slice(1));
 }
