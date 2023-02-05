7b4e97a0c341d97420fbe983587a322912fb1e67
JaredMathis <jared.mathis@native.consulting>
2023-02-05 04:21:21 -0500

    function_auto string_split

--- src/string/split.mjs
+++ src/string/split.mjs
@@ -1,2 +1,6 @@
-export function string_split() {
+import { arguments_assert } from './../arguments/assert.mjs';
+import { string_is } from './is.mjs';
+export function string_split(s, split_by) {
+    arguments_assert(arguments, string_is, string_is);
+    return s.split(split_by);
 }
