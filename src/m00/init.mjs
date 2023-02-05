3a678fd64b83785647eb5556ea9bbdcb287c998e
JaredMathis <jared.mathis@native.consulting>
2023-02-04 19:43:28 -0500

    function_auto m00_init

--- src/m00/init.mjs
+++ src/m00/init.mjs
@@ -1,16 +1,16 @@
+import { directory_create_if_not_exists } from './../directory/create/if/not/exists.mjs';
 import { run } from './../run.mjs';
 import { package_scripts } from './../package/scripts.mjs';
 import { gitignore_node_modules } from './../gitignore/node/modules.mjs';
 import { directory_source_get } from './../directory/source/get.mjs';
 import { index_new } from './../index/new.mjs';
-import { file_directory_create_if_not_exists } from '../file/directory/create/if/not/exists.mjs';
 export async function m00_init() {
     await gitignore_node_modules();
     await package_scripts();
-    await file_directory_create_if_not_exists(directory_source_get());
+    await directory_create_if_not_exists(directory_source_get());
     if (false) {
         await run(`npm install webpack webpack-cli --save-dev`);
-        await file_directory_create_if_not_exists('public');
+        await directory_create_if_not_exists('public');
         await index_new();
     }
 }
