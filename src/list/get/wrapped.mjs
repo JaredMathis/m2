40bb26c3da9cd009122dfe59f684613ea6096801
JaredMathis <jared.mathis@native.consulting>
2023-02-04 20:12:31 -0500

    function_auto list_get_wrapped

--- src/list/get/wrapped.mjs
+++ src/list/get/wrapped.mjs
@@ -1,3 +1,6 @@
-export function list_get_wrapped() {
-    
+export function list_get_wrapped(list, index) {
+    let modded = index % list.length;
+    modded += list.length;
+    modded %= list.length;
+    return list[modded];
 }
