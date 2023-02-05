c355f88bbf19267f653a2837ff3e06a6f175571c
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:18:04 -0500

    function_auto list_index_of

--- src/list/index/of.mjs
+++ src/list/index/of.mjs
@@ -1,6 +1,7 @@
+import { tautology } from './../../tautology.mjs';
 import { arguments_assert } from './../../arguments/assert.mjs';
 import { list_is } from './../is.mjs';
 export function list_index_of(list, item) {
-    arguments_assert(arguments, list_is);
+    arguments_assert(arguments, list_is, tautology);
     return list.indexOf(item);
 }
