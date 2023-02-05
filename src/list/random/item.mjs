2a1d46247255ce710a7987ec011f2340c3b056f8
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:50:07 -0500

    function_auto list_random_item

--- src/list/random/item.mjs
+++ src/list/random/item.mjs
@@ -1,2 +1,6 @@
-export function list_random_item() {
+import { arguments_assert } from './../../arguments/assert.mjs';
+import { list_is } from './../is.mjs';
+export function list_random_item(items) {
+    arguments_assert(arguments, list_is);
+    return items[Math.floor(Math.random() * items.length)];
 }
