2455c5b95c89e28951754d4761662f74320e8678
JaredMathis <jared.mathis@native.consulting>
2023-02-05 08:06:08 -0500

    function_auto list_single

--- src/list/single.mjs
+++ src/list/single.mjs
@@ -1,2 +1,9 @@
-export function list_single() {
+import { arguments_assert } from './../arguments/assert.mjs';
+import { assert } from './../assert.mjs';
+import { list_first } from './first.mjs';
+import { list_is } from './is.mjs';
+export function list_single(list) {
+    arguments_assert(arguments, list_is);
+    assert(list.length === 1);
+    return list_first(list);
 }
