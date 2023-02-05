1b417f1b33836f5ac6c52870aad8a1bcc1cd9ac2
JaredMathis <jared.mathis@native.consulting>
2023-02-04 20:04:40 -0500

    package_publish

--- src/number/at/least.mjs
+++ src/number/at/least.mjs
@@ -3,6 +3,6 @@ import { arguments_assert } from './../../arguments/assert.mjs';
 export function number_at_least(at_least) {
     arguments_assert(arguments, number_is);
     return function number_at_least_inner(n) {
-        return n >= at_least;
+        return number_is(n) && n >= at_least;
     };
 }
