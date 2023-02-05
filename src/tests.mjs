57d9289584a8f8c411656634f3137c1c54b5ec1e
JaredMathis <jared.mathis@native.consulting>
2023-02-05 10:24:38 -0500

    function_auto_run tests

--- src/tests.mjs
+++ src/tests.mjs
@@ -2,5 +2,6 @@ import { log } from './log.mjs';
 import { functions_all_get } from './functions/all/get.mjs';
 export async function tests() {
     let f = await functions_all_get();
-    console.log(f);
+    let filtered = f.filter(f => f.endsWith('_test'));
+    console.log(filtered);
 }
