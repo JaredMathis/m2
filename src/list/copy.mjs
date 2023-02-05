4b4e086c78512256f5b652997363a8a544bf0fe0
JaredMathis <jared.mathis@native.consulting>
2023-02-04 20:06:32 -0500

    package_publish

--- src/list/copy.mjs
+++ src/list/copy.mjs
@@ -1,3 +1,7 @@
-export function list_copy() {
-    
+import { arguments_assert } from "../arguments/assert.mjs";
+import { list_is } from "./is.mjs";
+
+export function list_copy(list) {
+    arguments_assert(arguments, list_is);
+    return list.slice()
 }
