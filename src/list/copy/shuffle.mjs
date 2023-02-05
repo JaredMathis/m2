93fcfa3063627bbe464aa78e3a5d9376026ae755
JaredMathis <jared.mathis@native.consulting>
2023-02-04 23:43:25 -0500

    function_auto list_copy_shuffle

--- src/list/copy/shuffle.mjs
+++ src/list/copy/shuffle.mjs
@@ -1,2 +1,8 @@
-export function list_copy_shuffle() {
+import { list_copy } from './../copy.mjs';
+import { list_shuffle } from './../shuffle.mjs';
+export function list_copy_shuffle(list) {
+    let result = list;
+    result = list_copy(result);
+    result = list_shuffle(result);
+    return result;
 }
