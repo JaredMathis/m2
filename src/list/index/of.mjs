baed480cdb1b621f795721bc9bb3bdc2874ba241
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:16:20 -0500

    function_auto list_index_of

--- src/list/index/of.mjs
+++ src/list/index/of.mjs
@@ -1,3 +1,6 @@
+import { arguments_assert } from './../../arguments/assert.mjs';
+import { list_is } from './../is.mjs';
 export function list_index_of(list, item) {
+    arguments_assert(arguments, list_is);
     return list.indexOf(item);
 }
