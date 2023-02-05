90fa7cabba1e71a99a3fe3f34a1c7ebb75dbea76
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:16:47 -0500

    refactor_call_to_function indexOf list_index_of

--- src/list/remove.mjs
+++ src/list/remove.mjs
@@ -1,6 +1,7 @@
+import { list_index_of } from './index/of.mjs';
 import { assert } from './../assert.mjs';
 export function list_remove(list, item) {
-    let index = list.indexOf(item);
+    let index = list_index_of(list, item);
     assert(index >= 0);
     list.splice(index, 1);
 }
