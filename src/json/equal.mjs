1cb66f226188f22da239a43e49af83313f3defda
JaredMathis <jared.mathis@native.consulting>
2023-02-05 08:26:53 -0500

    function_auto json_equal

--- src/json/equal.mjs
+++ src/json/equal.mjs
@@ -1,2 +1,7 @@
-export function json_equal() {
+import { arguments_assert } from './../arguments/assert.mjs';
+import { defined_is } from './../defined/is.mjs';
+import { json_to } from './to.mjs';
+export function json_equal(a, b) {
+    arguments_assert(arguments, defined_is, defined_is);
+    return json_to(a) === json_to(b);
 }
