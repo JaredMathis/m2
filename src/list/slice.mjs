6491dc08487236556aede3d871f918a0d1edf6a1
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:29:41 -0500

    package_publish_vs_code_open ../learn

--- src/list/slice.mjs
+++ src/list/slice.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert } from './../arguments/assert.mjs';
 import { number_negative_not } from './../number/negative/not.mjs';
+import { list_is } from './is.mjs';
 export function list_slice(list, from, to) {
-    arguments_assert(arguments, number_negative_not, number_negative_not);
+    arguments_assert(arguments, list_is, number_negative_not, number_negative_not);
     return list.slice(from, to);
 }
