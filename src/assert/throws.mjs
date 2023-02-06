d83831facc675484706a5e3b62d26db60f73a7e5
JaredMathis <jared.mathis@native.consulting>
2023-02-05 21:38:19 -0500

    function_auto assert_throws

--- src/assert/throws.mjs
+++ src/assert/throws.mjs
@@ -1,2 +1,10 @@
-export function assert_throws() {
+import { assert } from './../assert.mjs';
+export function assert_throws(lambda) {
+    let success = false;
+    try {
+        lambda();
+    } catch (e) {
+        success = true;
+    }
+    assert(success);
 }
