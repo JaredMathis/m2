94527ee1981bb642a680021ebbfbe78c32d29b95
JaredMathis <jared.mathis@native.consulting>
2023-02-05 21:28:00 -0500

    package_publish_vs_code_open ../learn

--- src/function/auto/tests.mjs
+++ src/function/auto/tests.mjs
@@ -1,2 +1,7 @@
-export function function_auto_tests() {
+import { tests } from "../../tests.mjs";
+import { function_auto } from "../auto.mjs";
+
+export function function_auto_tests(function_name) {
+    await function_auto(function_name);
+    await tests();
 }
