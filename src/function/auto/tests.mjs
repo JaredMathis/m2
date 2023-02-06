c25d34e433b97e344da9426605a0b9283ec68f1d
JaredMathis <jared.mathis@native.consulting>
2023-02-05 21:28:39 -0500

    function_auto function_auto_tests

--- src/function/auto/tests.mjs
+++ src/function/auto/tests.mjs
@@ -1,7 +1,6 @@
-import { tests } from "../../tests.mjs";
-import { function_auto } from "../auto.mjs";
-
-export function function_auto_tests(function_name) {
+import { tests } from './../../tests.mjs';
+import { function_auto } from './../auto.mjs';
+export async function function_auto_tests(function_name) {
     await function_auto(function_name);
     await tests();
 }
