00ecb4ca51d719bb315c5ac8529518d4328745d7
JaredMathis <jared.mathis@native.consulting>
2023-02-06 02:38:03 -0500

    function_auto json_copy

--- src/json/copy.mjs
+++ src/json/copy.mjs
@@ -1,2 +1,6 @@
-export function json_copy() {
+import { json_from } from './from.mjs';
+import { json_to } from './to.mjs';
+export function json_copy(j) {
+    let json = json_to(j);
+    return json_from(json);
 }
