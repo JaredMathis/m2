468fc358edab9c3efe8d34c6f676173273965bb1
JaredMathis <jared.mathis@native.consulting>
2023-02-07 11:13:36 -0500

    function_auto index_html_new

--- src/index/html/new.mjs
+++ src/index/html/new.mjs
@@ -1,2 +1,11 @@
-export function index_html_new() {
+import { file_write } from './../../file/write.mjs';
+export async function index_html_new() {
+    await file_write(`public/index.html`, `
+<html>
+    <head></head>
+    <body>
+        <script type="module" src="main.js"></script>
+    </body>
+</html>
+`);
 }
