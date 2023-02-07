5bd49b3aa52b3e878b6b884163618fc4e8b3df16
JaredMathis <jared.mathis@native.consulting>
2023-02-07 11:18:16 -0500

    function_auto index_js_new

--- src/index/js/new.mjs
+++ src/index/js/new.mjs
@@ -1,8 +1,11 @@
-import { directory_source_get } from "../../directory/source/get.mjs";
-import { path_join } from "../../path/join.mjs";
-
-export function index_js_new() {
-    await file_write(path_join([directory_source_get(), `index.js`]) , `
+import { file_write } from './../../file/write.mjs';
+import { directory_source_get } from './../../directory/source/get.mjs';
+import { path_join } from './../../path/join.mjs';
+export async function index_js_new() {
+    await file_write(path_join([
+        directory_source_get(),
+        `index.js`
+    ]), `
 import main from './main.mjs';
 main();
 `);
