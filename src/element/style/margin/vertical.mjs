350b2f3f76008b0f391e301abc28597045e6595d
JaredMathis <jared.mathis@native.consulting>
2023-02-07 12:54:53 -0500

    function_auto element_style_margin_vertical_auto

--- src/element/style/margin/vertical.mjs
+++ src/element/style/margin/vertical.mjs
@@ -1,2 +1,8 @@
-export function element_style_margin_vertical() {
+import { element_style } from "../../style.mjs";
+
+export function element_style_margin_vertical(e, value) {
+    element_style(e, {
+        marginTop: value,
+        marginBottom: value,
+    })
 }
--- src/element/style/margin/vertical/auto.mjs
+++ src/element/style/margin/vertical/auto.mjs
@@ -1,2 +1,4 @@
-export function element_style_margin_vertical_auto() {
+import { element_style_margin_vertical } from './../vertical.mjs';
+export function element_style_margin_vertical_auto(e) {
+    element_style_margin_vertical(e, 'auto');
 }
