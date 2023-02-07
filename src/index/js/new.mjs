95248b24c5278aed57bbbe332a970611dd4613cb
JaredMathis <jared.mathis@native.consulting>
2023-02-07 11:16:35 -0500

    function_auto m00_init_web

--- src/index/js/new.mjs
+++ src/index/js/new.mjs
@@ -1,2 +1,9 @@
+import { directory_source_get } from "../../directory/source/get.mjs";
+import { path_join } from "../../path/join.mjs";
+
 export function index_js_new() {
+    await file_write(path_join([directory_source_get(), `index.js`]) , `
+import main from './main.mjs';
+main();
+`);
 }
--- src/m00/init/web.mjs
+++ src/m00/init/web.mjs
@@ -1,13 +1,14 @@
+import { index_js_new } from './../../index/js/new.mjs';
+import { function_auto } from './../../function/auto.mjs';
 import { index_html_new } from './../../index/html/new.mjs';
 import { m00_init } from './../init.mjs';
 import { run } from './../../run.mjs';
 import { directory_create_if_not_exists } from './../../directory/create/if/not/exists.mjs';
-import { arguments_assert } from './../../arguments/assert.mjs';
-import { string_is } from './../../string/is.mjs';
-export async function m00_init_web(prefix) {
-    arguments_assert(arguments, string_is);
+export async function m00_init_web() {
     await m00_init();
     await run(`npm install webpack webpack-cli --save-dev`);
     await directory_create_if_not_exists('public');
     await index_html_new();
+    await index_js_new();
+    await function_auto('main');
 }
