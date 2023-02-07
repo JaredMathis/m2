0e6c1e5db31eae7d036f15b10a3eee78c93401c8
JaredMathis <jared.mathis@native.consulting>
2023-02-07 11:10:12 -0500

    function_auto m00_init_web

--- src/m00/init/web.mjs
+++ src/m00/init/web.mjs
@@ -1,4 +1,8 @@
-export function m00_init_web() {
+import { m00_init } from './../init.mjs';
+import { run } from './../../run.mjs';
+import { directory_create_if_not_exists } from './../../directory/create/if/not/exists.mjs';
+import { index_new } from './../../index/new.mjs';
+export async function m00_init_web() {
     await m00_init();
     await run(`npm install webpack webpack-cli --save-dev`);
     await directory_create_if_not_exists('public');
