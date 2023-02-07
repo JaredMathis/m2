6f61d79336dbf677ad18a8906e86cb8995b302a1
JaredMathis <jared.mathis@native.consulting>
2023-02-07 12:05:05 -0500

    function_auto element_style_display_inline_block

--- src/element/style/display/inline/block.mjs
+++ src/element/style/display/inline/block.mjs
@@ -1,2 +1,4 @@
-export function element_style_display_inline_block() {
+import { element_style } from './../../../style.mjs';
+export function element_style_display_inline_block(e) {
+    element_style(e, { display: 'inline-block' });
 }
