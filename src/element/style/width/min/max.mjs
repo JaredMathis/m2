0ab48064185773455ef189097b4c78206fcad92d
JaredMathis <jared.mathis@native.consulting>
2023-02-07 12:02:57 -0500

    function_auto element_style_width_min_max

--- src/element/style/width/min/max.mjs
+++ src/element/style/width/min/max.mjs
@@ -1,2 +1,11 @@
-export function element_style_width_min_max() {
+import { arguments_assert } from './../../../../arguments/assert.mjs';
+import { defined_is } from './../../../../defined/is.mjs';
+import { string_is } from './../../../../string/is.mjs';
+import { element_style } from './../../../style.mjs';
+export function element_style_width_min_max(e, value) {
+    arguments_assert(arguments, defined_is, string_is);
+    element_style(e, {
+        minWidth: value,
+        maxWith: value
+    });
 }
