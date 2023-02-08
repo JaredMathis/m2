02aa011aaf1f03b89af05e15592bd5c164d957a0
JaredMathis <jared.mathis@native.consulting>
2023-02-07 23:25:33 -0500

    function_auto element_attribute_merge

--- src/element/attribute/merge.mjs
+++ src/element/attribute/merge.mjs
@@ -1,2 +1,7 @@
-export function element_attribute_merge() {
+import { for_each } from './../../for/each.mjs';
+import { element_attribute_set } from './set.mjs';
+export function element_attribute_merge(e, attributes) {
+    for_each(attributes, (attribute_value, attribute_key) => {
+        element_attribute_set(e, attribute_key, attribute_value);
+    });
 }
