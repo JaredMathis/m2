1b58c49a038f0e61c0f2ca970da70a0c469f617f
JaredMathis <jared.mathis@native.consulting>
2023-02-07 22:23:46 -0500

    function_auto window_match_media_on_change

--- src/window/match/media/on/change.mjs
+++ src/window/match/media/on/change.mjs
@@ -1,7 +1,8 @@
-import { arguments_assert } from "../../../../arguments/assert.mjs";
-import { string_is } from "../../../../string/is.mjs";
+import { function_is } from './../../../../function/is.mjs';
+import { arguments_assert } from './../../../../arguments/assert.mjs';
+import { string_is } from './../../../../string/is.mjs';
 export function window_match_media_on_change(media_query, on_change) {
-    arguments_assert(arguments, string_is, function_is)
+    arguments_assert(arguments, string_is, function_is);
     const mediaQueryList = window.matchMedia(media_query);
-    mediaQueryList.addEventListener('change', on_change)
+    mediaQueryList.addEventListener('change', on_change);
 }
