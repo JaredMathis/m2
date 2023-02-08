dc9abe1ed348a504474bb8e77fdee11d41ed036a
JaredMathis <jared.mathis@native.consulting>
2023-02-08 02:44:04 -0500

    package_publish_vs_code_open ../transport

--- src/click.mjs
+++ src/click.mjs
@@ -3,5 +3,5 @@ import { defined_is } from './defined/is.mjs';
 import { function_is } from './function/is.mjs';
 export function click(e, on_click) {
     arguments_assert(arguments, defined_is, function_is);
-    e.addEventListener(on_click);
+    e.addEventListener('click', on_click);
 }
