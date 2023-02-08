910e3a88957df26fc9030f5a3589d29eca43904d
JaredMathis <jared.mathis@native.consulting>
2023-02-07 23:52:08 -0500

    function_auto element_style_display_none

--- src/element/style/display/none.mjs
+++ src/element/style/display/none.mjs
@@ -1,6 +1,7 @@
+import { element_style } from './../../style.mjs';
 import { arguments_assert } from './../../../arguments/assert.mjs';
 import { defined_is } from './../../../defined/is.mjs';
 export function element_style_display_none(e) {
     arguments_assert(arguments, defined_is);
-    element_style_merge(e, { display: 'none' });
+    element_style(e, { display: 'none' });
 }
