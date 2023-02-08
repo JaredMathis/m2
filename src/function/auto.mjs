54d20e0c263a4aee271712b5ee032660e4e1fabd
JaredMathis <jared.mathis@native.consulting>
2023-02-08 05:50:42 -0500

    function_auto function_auto

--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -1,6 +1,6 @@
-import { function_imports } from './imports.mjs';
 import { function_open } from './open.mjs';
+import { function_split } from './split.mjs';
 export async function function_auto(function_name) {
     await function_open(function_name);
-    await function_imports(function_name);
+    await function_split(function_name);
 }
