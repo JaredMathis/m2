62d806d75ccee6d0b4d30a46b5eda604e2895276
JaredMathis <jared.mathis@native.consulting>
2023-02-08 06:02:08 -0500

    proress

--- src/test/a.mjs
+++ src/test/a.mjs
@@ -1,4 +1,16 @@
-import { test_e } from './e.mjs';
 export function test_a() {
     return test_e();
+}
+function test_e() {
+    return test_b() + test_c() + test_d();
+}
+
+function test_b() {
+    return 'b' + test_c() + test_d();
+}
+function test_c() {
+    return 'c' + test_d();
+}
+function test_d() {
+    return 'd';
 }
