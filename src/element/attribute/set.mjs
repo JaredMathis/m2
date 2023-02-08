da9ef006a7104f91714ef2477a535a40981a8eac
JaredMathis <jared.mathis@native.consulting>
2023-02-07 23:24:28 -0500

    function_auto element_attribute_set

--- src/element/attribute/set.mjs
+++ src/element/attribute/set.mjs
@@ -1,2 +1,7 @@
-export function element_attribute_set() {
+import { arguments_assert } from './../../arguments/assert.mjs';
+import { defined_is } from './../../defined/is.mjs';
+import { string_is } from './../../string/is.mjs';
+export function element_attribute_set(e, attribute, value) {
+    arguments_assert(arguments, defined_is, string_is, string_is);
+    e.setAttribute(attribute, value);
 }
