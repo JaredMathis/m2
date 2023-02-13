f9243663f158211c9605096fcc61c22c0db87cad
JaredMathis <jared.mathis@native.consulting>
2023-02-13 10:57:28 -0500

    function_auto directory_root_get

--- src/directory/root/get.mjs
+++ src/directory/root/get.mjs
@@ -2,8 +2,9 @@ import { m00_config_get } from './../../m00/config/get.mjs';
 import { directory_source_get } from './../source/get.mjs';
 import { m00_directory_root_get } from './../../m00/directory/root/get.mjs';
 export async function directory_root_get() {
-    if (false) {
-        let config = await m00_config_get();
+    let config = await m00_config_get();
+    if (config.directory_root) {
+        return config.directory_root;
     }
     return [
         m00_directory_root_get(),
