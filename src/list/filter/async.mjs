e86fe152961ff0da7970d19dddc0a01caaa273f6
JaredMathis <jared.mathis@native.consulting>
2023-02-13 12:07:59 -0500

    function_auto list_filter_async

--- src/list/filter/async.mjs
+++ src/list/filter/async.mjs
@@ -1,2 +1,11 @@
-export function list_filter_async() {
+import { list_add } from './../add.mjs';
+import { for_each_async } from './../../for/each/async.mjs';
+export async function list_filter_async(list, filter) {
+    let result = [];
+    await for_each_async(list, async item => {
+        if (await filter(item)) {
+            list_add(result, item);
+        }
+    });
+    return result;
 }
