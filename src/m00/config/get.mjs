c18c9e779495e86de9acf2d4410cd8657afb92d7
JaredMathis <jared.mathis@native.consulting>
2023-02-13 10:55:32 -0500

    function_auto m00_config_get

--- src/m00/config/get.mjs
+++ src/m00/config/get.mjs
@@ -1,2 +1,9 @@
-export function m00_config_get() {
+import { file_json_read } from './../../file/json/read.mjs';
+import { file_exists } from './../../file/exists.mjs';
+export async function m00_config_get() {
+    let config_path = `./m00_config.json`;
+    if (!await file_exists(config_path)) {
+        return {};
+    }
+    return await file_json_read(config_path);
 }
